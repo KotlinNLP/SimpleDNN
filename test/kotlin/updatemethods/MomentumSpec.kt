352bd7714885b18c0fe0c586d8be771cf845ab3e
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-23 11:51:58 +0200

    Add 'epoch scheduling' context tests

--- test/kotlin/updatemethods/MomentumSpec.kt
+++ test/kotlin/updatemethods/MomentumSpec.kt
@@ -7,10 +7,16 @@
 
 package updatemethods
 
+import com.kotlinnlp.simplednn.core.functionalities.decaymethods.DecayMethod
 import com.kotlinnlp.simplednn.core.functionalities.updatemethods.momentum.MomentumMethod
 import com.kotlinnlp.simplednn.core.functionalities.updatemethods.momentum.MomentumStructure
 import com.kotlinnlp.simplednn.simplemath.NDArray
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
@@ -47,6 +53,36 @@ class MomentumSpec: Spek({
         assertEquals(true, updateableArray.values.equals(
             NDArray.arrayOf(doubleArrayOf(-0.2309, -0.3207, 0.0496, 0.7292, 0.6199)),
           tolerance = 1.0e-5))
+      }
+    }
+
+    context("epoch scheduling") {
+
+      val decayMethod = mock<DecayMethod>()
+      whenever(decayMethod.update(learningRate = any(), timeStep = eq(1))).thenReturn(0.03)
+      whenever(decayMethod.update(learningRate = any(), timeStep = eq(2))).thenReturn(0.05)
+
+      on("first epoch") {
+
+        val updateHelper = MomentumMethod(learningRate = 0.001, decayMethod = decayMethod)
+
+        updateHelper.newEpoch()
+
+        it("should match the expected alpha in the first epoch") {
+          assertEquals(0.03, updateHelper.alpha)
+        }
+      }
+
+      on("second epoch") {
+
+        val updateHelper = MomentumMethod(learningRate = 0.001, decayMethod = decayMethod)
+
+        updateHelper.newEpoch()
+        updateHelper.newEpoch()
+
+        it("should match the expected alpha in the second epoch") {
+          assertEquals(0.05, updateHelper.alpha)
+        }
       }
     }
   }
