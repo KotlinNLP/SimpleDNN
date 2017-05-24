2d8613c2c64eb1be0d2579109094737640c67dd6
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-24 17:35:24 +0200

    Convert to a concrete object

--- src/kotlin/com/kotlinnlp/simplednn/simplemath/ndarray/NDArrayFactory.kt
+++ src/kotlin/com/kotlinnlp/simplednn/simplemath/ndarray/NDArrayFactory.kt
@@ -7,51 +7,97 @@
 
 package com.kotlinnlp.simplednn.simplemath.ndarray
 
+import org.jblas.DoubleMatrix
+
 /**
  *
  */
-interface NDArrayFactory {
+object NDArrayFactory {
+
+  /**
+   * @param shape shape
+   * @return a new empty DenseNDArray
+   */
+  fun emptyArray(shape: Shape): NDArray {
+    return DenseNDArray(DoubleMatrix.zeros(shape.dim1, shape.dim2))
+  }
+
+  /**
+   *
+   */
+  fun arrayOf(vector: DoubleArray): NDArray {
+    val m = DoubleMatrix(vector.size, 1)
+
+    (0 until vector.size).forEach { i -> m.put(i, vector[i]) }
+
+    return DenseNDArray(m)
+  }
+
+  /**
+   *
+   */
+  fun arrayOf(matrix: Array<DoubleArray>): NDArray {
+    val rows = matrix.size
+    val columns = matrix[0].size
+    val m = DoubleMatrix(rows, columns)
+
+    (0 until rows * columns).forEach { linearIndex ->
+      // linear indexing: loop rows before, column by column
+      val row = linearIndex % rows
+      val column = linearIndex / rows
+      m.put(linearIndex, matrix[row][column])
+    }
+
+    return DenseNDArray(m)
+  }
 
   /**
    *
    * @param shape shape
-   * @return a new empty NDArray
+   * @return a new DenseNDArray filled with zeros
    */
-  fun emptyArray(shape: Shape): NDArrayInterface
+  fun zeros(shape: Shape): NDArray {
+    return this.emptyArray(shape)
+  }
 
   /**
-   *
-   */
-  fun arrayOf(vector: DoubleArray): NDArrayInterface
-
-  /**
-   *
-   */
-  fun arrayOf(matrix: Array<DoubleArray>): NDArrayInterface
-
-  /**
-   *
-   * @param shape shape
-   * @return a new NDArray filled with zeros
-   */
-  fun zeros(shape: Shape): NDArrayInterface
-
-  /**
-   * Build a new NDArrayInterface filled with zeros but one with 1.0
+   * Build a new DenseNDArray filled with zeros but one with 1.0
    *
    * @param length the length of the array
    * @param oneAt the index of the one element
-   * @return a oneHotEncoder NDArrayInterface
+   * @return a oneHotEncoder DenseNDArray
    */
-  fun oneHotEncoder(length: Int, oneAt: Int): NDArrayInterface
+  fun oneHotEncoder(length: Int, oneAt: Int): NDArray {
+    require(oneAt in 0 until length)
+
+    val array = this.emptyArray(Shape(length))
+
+    array[oneAt] = 1.0
+
+    return array
+  }
 
   /**
-   * Build a new NDArrayInterface filled with random values uniformly distributed in range [[from], [to])
+   * Build a new DenseNDArray filled with random values uniformly distributed in range [[from], [to]]
    *
    * @param shape shape
    * @param from inclusive lower bound of random values range
-   * @param to exclusive upper bound of random values range
-   * @return a new NDArray filled with random values
+   * @param to inclusive upper bound of random values range
+   * @return a new DenseNDArray filled with random values
    */
-  fun random(shape: Shape, from: Double = 0.0, to: Double = 1.0): NDArrayInterface
+  fun random(shape: Shape, from: Double, to: Double): NDArray {
+
+    val m = DoubleMatrix.rand(shape.dim1, shape.dim2)
+    val rangeSize = to - from
+
+    if (rangeSize != 1.0) {
+      m.muli(rangeSize)
+    }
+
+    if (from != 0.0) {
+      m.addi(from)
+    }
+
+    return DenseNDArray(m)
+  }
 }
