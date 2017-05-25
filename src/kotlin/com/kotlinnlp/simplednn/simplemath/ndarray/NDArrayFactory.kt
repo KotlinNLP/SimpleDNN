605a2370706934592761fc1ec430f0494f0b524c
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-25 17:13:02 +0200

    Update documentation

--- src/kotlin/com/kotlinnlp/simplednn/simplemath/ndarray/NDArrayFactory.kt
+++ src/kotlin/com/kotlinnlp/simplednn/simplemath/ndarray/NDArrayFactory.kt
@@ -20,9 +20,11 @@ interface NDArrayFactory<NDArrayType : NDArray<NDArrayType>> {
   fun emptyArray(shape: Shape): NDArrayType
 
   /**
+   * Build a new [NDArrayType] filled with zeros
    *
    * @param shape shape
-   * @return a new [NDArrayType] filled with zeros
+   *
+   * @return a new [NDArrayType]
    */
   fun zeros(shape: Shape): NDArrayType
 
@@ -31,6 +33,7 @@ interface NDArrayFactory<NDArrayType : NDArray<NDArrayType>> {
    *
    * @param length the length of the array
    * @param oneAt the index of the one element
+   *
    * @return a oneHotEncoder [NDArrayType]
    */
   fun oneHotEncoder(length: Int, oneAt: Int): NDArrayType
@@ -41,6 +44,7 @@ interface NDArrayFactory<NDArrayType : NDArray<NDArrayType>> {
    * @param shape shape
    * @param from inclusive lower bound of random values range
    * @param to inclusive upper bound of random values range
+   *
    * @return a new [NDArrayType] filled with random values
    */
   fun random(shape: Shape, from: Double = 0.0, to: Double = 1.0): NDArrayType
