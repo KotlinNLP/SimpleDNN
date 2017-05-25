ae76acf07e717f6e6960df8d331e9b1932490a1c
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-25 16:55:42 +0200

    Add UpdatableDenseArray

---
+++ src/kotlin/com/kotlinnlp/simplednn/core/arrays/UpdatableDenseArray.kt
@@ -0,0 +1,32 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.arrays
+
+import com.kotlinnlp.simplednn.simplemath.ndarray.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.DenseNDArrayFactory
+import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+
+/**
+ * The [UpdatableDenseArray] is a wrapper of a [DenseNDArray]
+ */
+class UpdatableDenseArray(override val values: DenseNDArray) : UpdatableArray(values = values) {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed from Serializable)
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+
+    /**
+     *
+     */
+    operator fun invoke(shape: Shape) = UpdatableArray(DenseNDArrayFactory.zeros(shape))
+  }
+}
