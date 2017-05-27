873f77a26221390adc249cf0fbcd396ba42cc414
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-27 11:23:19 +0200

    Update usage of NDArray adding generic type handling

--- src/kotlin/com/kotlinnlp/simplednn/deeplearning/birnn/BiRNNUtils.kt
+++ src/kotlin/com/kotlinnlp/simplednn/deeplearning/birnn/BiRNNUtils.kt
@@ -7,8 +7,8 @@
 
 package com.kotlinnlp.simplednn.deeplearning.birnn
 
-import com.kotlinnlp.simplednn.simplemath.NDArray
 import com.kotlinnlp.simplednn.simplemath.concatVectorsV
+import com.kotlinnlp.simplednn.simplemath.ndarray.DenseNDArray
 
 /**
  * BiRNNUtils contains functions which help to split and concatenate
@@ -23,10 +23,11 @@ object BiRNNUtils {
    *
    * @return a (array<left-to-right>, array<right-to-left>) errors Pair
    */
-  fun splitErrorsSequence(array: Array<NDArray>): Pair<Array<NDArray>, Array<NDArray>> {
+  fun splitErrorsSequence(array: Array<DenseNDArray>):
+    Pair<Array<DenseNDArray>, Array<DenseNDArray>> {
 
-    val leftToRightOutputErrors = arrayOfNulls<NDArray>(array.size)
-    val rightToLeftOutputErrors = arrayOfNulls<NDArray>(array.size)
+    val leftToRightOutputErrors = arrayOfNulls<DenseNDArray>(array.size)
+    val rightToLeftOutputErrors = arrayOfNulls<DenseNDArray>(array.size)
 
     array.indices.forEach { i -> val (a, b) = this.splitErrors(array[i])
       leftToRightOutputErrors[i] = a
@@ -43,7 +44,7 @@ object BiRNNUtils {
    *
    * @return a (left-to-right, right-to-left) errors Pair
    */
-  fun splitErrors(array: NDArray) = Pair(
+  fun splitErrors(array: DenseNDArray) = Pair(
     array.getRange(0, array.length / 2),
     array.getRange(array.length / 2, array.length))
 
@@ -55,7 +56,9 @@ object BiRNNUtils {
    *
    * @return the sum of the left-to-right and right-to-left errors
    */
-  fun sumBidirectionalErrors(leftToRightInputErrors: Array<NDArray>, rightToLeftInputErrors: Array<NDArray>): Array<NDArray> {
+  fun sumBidirectionalErrors(leftToRightInputErrors: Array<DenseNDArray>, rightToLeftInputErrors: Array<DenseNDArray>):
+    Array<DenseNDArray> {
+
     require(leftToRightInputErrors.size == rightToLeftInputErrors.size)
 
     return Array(size = leftToRightInputErrors.size, init = {
@@ -73,7 +76,7 @@ object BiRNNUtils {
    *
    * @return the array resulting from the concatenation
    */
-  fun concatenate(a: Array<NDArray>, b: Array<NDArray>): Array<NDArray> {
+  fun concatenate(a: Array<DenseNDArray>, b: Array<DenseNDArray>): Array<DenseNDArray> {
     require(a.size == b.size)
     return Array(size = a.size, init = { concatVectorsV(a[it], b[it]) })
   }
