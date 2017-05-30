09fb970652fcf7535a2c330d10ac80a16ba93b8a
KotlinNLP Authors <github@kotlinnlp.com>
2017-05-30 16:18:46 +0200

    Add abstract protected methods optimizeSparseErrors() and optimizeDenseErrors()

--- src/kotlin/com/kotlinnlp/simplednn/core/functionalities/updatemethods/UpdateMethod.kt
+++ src/kotlin/com/kotlinnlp/simplednn/core/functionalities/updatemethods/UpdateMethod.kt
@@ -11,6 +11,8 @@ import com.kotlinnlp.simplednn.core.arrays.UpdatableDenseArray
 import com.kotlinnlp.simplednn.core.functionalities.regularization.WeightsRegularization
 import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
 import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.sparse.SparseNDArray
 
 /**
  * UpdateMethod implements different gradient-based optimization algorithm (e.g. LearningRate, Adagrad, ADAM).
@@ -52,15 +54,50 @@ abstract class UpdateMethod(val regularization: WeightsRegularization?) {
   }
 
   /**
+   * Optimize the errors.
    *
-   * @param errors errors
-   * @param array parameter
-   * @return
+   * @param errors the errors to optimize (sparse or dense)
+   * @param array an [UpdatableDenseArray]
+   *
+   * @return optimized errors
    */
-  abstract protected fun <NDArrayType: NDArray<NDArrayType>> optimizeErrors(
+  protected fun <NDArrayType: NDArray<NDArrayType>> optimizeErrors(
     errors: NDArrayType,
     array: UpdatableDenseArray
-  ): NDArrayType
+  ): NDArrayType = when (errors) {
+
+    is SparseNDArray -> { // errors are Sparse when the input is SparseBinary
+      @Suppress("UNCHECKED_CAST")
+      this.optimizeSparseErrors(errors = errors, array = array) as NDArrayType
+    }
+
+    is DenseNDArray -> { // errors are Dense when the input is Dense
+      @Suppress("UNCHECKED_CAST")
+      this.optimizeDenseErrors(errors = errors, array = array) as NDArrayType
+    }
+
+    else -> throw RuntimeException("Invalid errors type")
+  }
+
+  /**
+   * Optimize sparse errors.
+   *
+   * @param errors the [SparseNDArray] errors to optimize
+   * @param array an [UpdatableDenseArray]
+   *
+   * @return optimized sparse errors
+   */
+  abstract protected fun optimizeSparseErrors(errors: SparseNDArray, array: UpdatableDenseArray): SparseNDArray
+
+  /**
+   * Optimize dense errors.
+   *
+   * @param errors the [DenseNDArray] errors to optimize
+   * @param array an [UpdatableDenseArray]
+   *
+   * @return optimized dense errors
+   */
+  abstract protected fun optimizeDenseErrors(errors: DenseNDArray, array: UpdatableDenseArray): DenseNDArray
 
   /**
    *
