2d68ff75b76b1d7584ce5e8efc838c35c75e9eeb
KotlinNLP Authors <github@kotlinnlp.com>
2017-06-13 18:19:43 +0200

    Add RecurrentRelevanceHelper

---
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/recurrent/RecurrentRelevanceHelper.kt
@@ -0,0 +1,29 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.layers.recurrent
+
+import com.kotlinnlp.simplednn.core.layers.LayerParameters
+import com.kotlinnlp.simplednn.core.layers.RelevanceHelper
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+
+/**
+ * The helper which calculates the relevance of the input of a [layer] respect of its output.
+ *
+ * @property layer the [RecurrentLayerStructure] in which to calculate the input relevance
+ */
+abstract class RecurrentRelevanceHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
+  layer: RecurrentLayerStructure<InputNDArrayType>
+) : RelevanceHelper<InputNDArrayType>(layer) {
+
+  /**
+   * Calculate the relevance of the output in the previous state respect of the current one.
+   *
+   * @param paramsContributes the contributes of the parameters during the last forward
+   */
+  abstract fun calculateRecurrentRelevance(paramsContributes: LayerParameters)
+}
