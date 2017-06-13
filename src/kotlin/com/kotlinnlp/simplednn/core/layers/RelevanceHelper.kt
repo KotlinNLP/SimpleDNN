462a2c05cf122bd19ce30f49bd7b12dcd11a799b
KotlinNLP Authors <github@kotlinnlp.com>
2017-06-13 13:43:16 +0200

    Convert to an abstract class containing concrete methods

--- src/kotlin/com/kotlinnlp/simplednn/core/layers/RelevanceHelper.kt
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/RelevanceHelper.kt
@@ -7,22 +7,141 @@
 
 package com.kotlinnlp.simplednn.core.layers
 
+import com.kotlinnlp.simplednn.core.arrays.Norm1Array
 import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import com.kotlinnlp.simplednn.simplemath.ndarray.sparse.SparseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.sparsebinary.SparseBinaryNDArray
 
 /**
  * The helper which calculates the relevance of the input of a [layer] respect of its output.
+ *
+ * @property layer the [LayerStructure] in which to calculate the input relevance
  */
-interface RelevanceHelper<InputNDArrayType : NDArray<InputNDArrayType>> {
+abstract class RelevanceHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
+  protected val layer: LayerStructure<InputNDArrayType>
+) {
 
   /**
-   * The [LayerStructure] in which to calculate the input relevance.
+   * The stabilizing term used to calculate the relevance
    */
-  val layer: LayerStructure<InputNDArrayType>
+  private val relevanceEps: Double = 0.01
 
   /**
    * Calculate the relevance of the input.
    *
    * @param paramsContributes the contributes of the parameters during the last forward
    */
-  fun calculateRelevance(paramsContributes: LayerParameters)
+  abstract fun calculateRelevance(paramsContributes: LayerParameters)
+
+  /**
+   * Calculate the relevance of the array [x] respect of the calculation which produced the array [y].
+   *
+   * @param x a generic [NDArray]
+   * @param y a [DenseNDArray] (no Sparse needed, generally little size on output)
+   * @param yRelevance a [DenseNDArray], whose norm is 1.0, which indicates how much relevant are the values of [y]
+   * @param contributes a matrix which contains the contributes of each value of [x] to calculate each value of [y]
+   *
+   * @return the relevance of [x] respect of [y]
+   */
+  protected fun calculateRelevanceOfArray(x: InputNDArrayType,
+                                          y: DenseNDArray,
+                                          yRelevance: DenseNDArray,
+                                          contributes: NDArray<*>) : Norm1Array<*> =
+    when (x) {
+
+      is DenseNDArray -> this.calculateRelevanceOfDenseArray(
+        x = x,
+        y = y,
+        yRelevance = yRelevance,
+        contributes = contributes as DenseNDArray)
+
+      is SparseBinaryNDArray -> this.calculateRelevanceOfSparseArray(
+        x = x,
+        y = y,
+        yRelevance = yRelevance,
+        contributes = contributes as SparseNDArray)
+
+      else -> throw RuntimeException("Invalid input type '%s'".format(x.javaClass.name))
+    }
+
+  /**
+   * Calculate the relevance of the Dense array [x] respect of the calculation which produced the Dense array [y].
+   *
+   * @param x a [DenseNDArray]
+   * @param y a [DenseNDArray] (no Sparse needed, generally little size on output)
+   * @param yRelevance a [DenseNDArray], whose norm is 1.0, which indicates how much relevant are the values of [y]
+   * @param contributes a matrix which contains the contributes of each value of [x] to calculate each value of [y]
+   *
+   * @return the relevance of [x] respect of [y]
+   */
+  protected fun calculateRelevanceOfDenseArray(x: DenseNDArray,
+                                               y: DenseNDArray,
+                                               yRelevance: DenseNDArray,
+                                               contributes: DenseNDArray) : Norm1Array<DenseNDArray> {
+
+    val relevanceArray: DenseNDArray = DenseNDArrayFactory.zeros(shape = Shape(this.layer.inputArray.size))
+    val xLength: Int = x.length
+    val yLength: Int = y.length
+
+    for (i in 0 until xLength) {
+
+      for (j in 0 until yLength) {
+        val eps: Double = if (y[j] >= 0) this.relevanceEps else -this.relevanceEps
+        val epsN: Double = eps / xLength
+
+        relevanceArray[i] += yRelevance[j] * (contributes[j, i]  + epsN) / (y[j] + eps)
+      }
+    }
+
+    return Norm1Array(values = relevanceArray)
+  }
+
+  /**
+   * Calculate the relevance of the SparseBinary array [x] respect of the calculation which produced the Dense array
+   * [y].
+   *
+   * @param x a [SparseBinaryNDArray]
+   * @param y a [DenseNDArray] (no Sparse needed, generally little size on output)
+   * @param yRelevance a [DenseNDArray], whose norm is 1.0, which indicates how much relevant are the values of [y]
+   * @param contributes a matrix which contains the contributes of each value of [x] to calculate each value of [y]
+   *
+   * @return the relevance of [x] respect of [y]
+   */
+  private fun calculateRelevanceOfSparseArray(x: SparseBinaryNDArray,
+                                              y: DenseNDArray,
+                                              yRelevance: DenseNDArray,
+                                              contributes: SparseNDArray) : Norm1Array<SparseNDArray> {
+
+    val xActiveIndices: List<Int> = x.activeIndicesByColumn.values.first()!!
+    val xActiveIndicesSize: Int =  xActiveIndices.size
+    val relevanceValues: Array<Double> = Array(size = xActiveIndicesSize, init = { 0.0 })
+    val relevanceColumns: Array<Int> = Array(size = xActiveIndicesSize, init = { 0 })
+    val relevanceRows: Array<Int> = xActiveIndices.toTypedArray()
+    val yLength: Int = y.length
+    var k: Int = 0
+
+    for (l in 0 until xActiveIndicesSize) {
+      // the indices of the non-zero elements of x are the same of the non-zero columns of contributes
+      for (j in 0 until yLength) {
+        // loop over the i-th column of contributes (which is non-zero)
+        val eps: Double = if (y[j] >= 0) this.relevanceEps else -this.relevanceEps
+        val epsN: Double = eps / xActiveIndicesSize
+        val wContrJI: Double = contributes.values[k++]  // linear indexing
+
+        relevanceValues[l] += yRelevance[j] * (wContrJI + epsN) / (y[j] + eps)
+      }
+    }
+
+    return Norm1Array(
+      values = SparseNDArray(
+        shape = x.shape,
+        values = relevanceValues,
+        rows = relevanceRows,
+        columns = relevanceColumns
+      )
+    )
+  }
 }
