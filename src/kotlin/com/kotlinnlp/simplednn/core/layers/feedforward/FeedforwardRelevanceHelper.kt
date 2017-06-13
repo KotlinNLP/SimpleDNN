a3f7f51ac5f13215d1bb3a52cf90b838b79494c3
KotlinNLP Authors <github@kotlinnlp.com>
2017-06-13 13:43:53 +0200

    Fix constructor, fix super class methods usage

--- src/kotlin/com/kotlinnlp/simplednn/core/layers/feedforward/FeedforwardRelevanceHelper.kt
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/feedforward/FeedforwardRelevanceHelper.kt
@@ -7,15 +7,10 @@
 
 package com.kotlinnlp.simplednn.core.layers.feedforward
 
-import com.kotlinnlp.simplednn.core.arrays.Norm1Array
 import com.kotlinnlp.simplednn.core.layers.LayerParameters
 import com.kotlinnlp.simplednn.core.layers.RelevanceHelper
 import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
-import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
-import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
-import com.kotlinnlp.simplednn.simplemath.ndarray.sparse.SparseNDArray
-import com.kotlinnlp.simplednn.simplemath.ndarray.sparsebinary.SparseBinaryNDArray
 
 /**
  * The helper which calculates the relevance of the input of a [layer] respect of its output.
@@ -23,13 +18,8 @@ import com.kotlinnlp.simplednn.simplemath.ndarray.sparsebinary.SparseBinaryNDArr
  * @property layer the [FeedforwardLayerStructure] in which to calculate the input relevance
  */
 class FeedforwardRelevanceHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
-  override val layer: FeedforwardLayerStructure<InputNDArrayType>
-) : RelevanceHelper<InputNDArrayType> {
-
-  /**
-   * The stabilizing term used to calculate the relevance
-   */
-  private val relevanceEps: Double = 0.01
+  layer: FeedforwardLayerStructure<InputNDArrayType>
+) : RelevanceHelper<InputNDArrayType>(layer) {
 
   /**
    * Calculate the relevance of the input.
@@ -40,89 +30,13 @@ class FeedforwardRelevanceHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
     this.layer.params as FeedforwardLayerParameters
     paramsContributes as FeedforwardLayerParameters
 
-    val x: InputNDArrayType = this.layer.inputArray.values
-    val y: DenseNDArray = this.layer.outputArray.valuesNotActivated
-    val yRelevance: DenseNDArray = this.layer.outputArray.relevance.values as DenseNDArray
-    // output relevance is always Dense (no Sparse needed, generally little size)
+    val inputRelevance = this.calculateRelevanceOfArray(
+      x = this.layer.inputArray.values,
+      y = this.layer.outputArray.valuesNotActivated,
+      yRelevance = this.layer.outputArray.relevance.values as DenseNDArray,
+      contributes = paramsContributes.weights.values
+    )
 
-    val contr: NDArray<*> = paramsContributes.weights.values
-
-    when (x) {
-
-      is DenseNDArray -> this.calculateRelevanceOfDenseInput(
-        x = x,
-        y = y,
-        yRelevance = yRelevance,
-        contr = contr as DenseNDArray)
-
-      is SparseBinaryNDArray -> this.calculateRelevanceOfSparseInput(
-        x = x,
-        y = y,
-        yRelevance = yRelevance,
-        contr = contr as SparseNDArray)
-
-      else -> throw RuntimeException("Invalid input type '%s'".format(x.javaClass.name))
-    }
-  }
-
-  /**
-   *
-   */
-  private fun calculateRelevanceOfDenseInput(x: DenseNDArray,
-                                             y: DenseNDArray,
-                                             yRelevance: DenseNDArray,
-                                             contr: DenseNDArray) {
-
-    val relevanceArray: DenseNDArray = DenseNDArrayFactory.zeros(shape = Shape(this.layer.inputArray.size))
-    val xLength: Int = x.length
-    val yLength: Int = y.length
-
-    for (i in 0 until xLength) {
-
-      for (j in 0 until yLength) {
-        val eps: Double = if (y[j] >= 0) this.relevanceEps else -this.relevanceEps
-        val epsN: Double = eps / xLength
-
-        relevanceArray[i] += yRelevance[j] * (contr[j, i]  + epsN) / (y[j] + eps)
-      }
-    }
-
-    this.layer.inputArray.assignRelevance(Norm1Array(values = relevanceArray))
-  }
-
-  /**
-   *
-   */
-  private fun calculateRelevanceOfSparseInput(x: SparseBinaryNDArray,
-                                              y: DenseNDArray,
-                                              yRelevance: DenseNDArray,
-                                              contr: SparseNDArray) {
-
-    val xActiveIndices: List<Int> = x.activeIndicesByColumn.values.first()!!
-    val xActiveIndicesSize: Int =  xActiveIndices.size
-    val relevanceValues: Array<Double> = Array(size = xActiveIndicesSize, init = { 0.0 })
-    val relevanceColumns: Array<Int> = Array(size = xActiveIndicesSize, init = { 0 })
-    val relevanceRows: Array<Int> = xActiveIndices.toTypedArray()
-    val yLength: Int = y.length
-    var k: Int = 0
-
-    for (l in 0 until xActiveIndicesSize) {
-      // the indices of the non-zero elements of x are the same of the non-zero columns of contr
-      for (j in 0 until yLength) {
-        // loop over the i-th column of contr (which is non-zero)
-        val eps: Double = if (y[j] >= 0) this.relevanceEps else -this.relevanceEps
-        val epsN: Double = eps / xActiveIndicesSize
-        val wContrJI: Double = contr.values[k++]  // linear indexing
-
-        relevanceValues[l] += yRelevance[j] * (wContrJI + epsN) / (y[j] + eps)
-      }
-    }
-
-    this.layer.inputArray.assignRelevance(Norm1Array(values = SparseNDArray(
-      shape = x.shape,
-      values = relevanceValues,
-      rows = relevanceRows,
-      columns = relevanceColumns
-    )))
+    this.layer.inputArray.assignRelevance(inputRelevance)
   }
 }
