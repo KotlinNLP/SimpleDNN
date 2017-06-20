16c2ba39f6ce11fc3378cadc17ef4c08dee702d7
KotlinNLP Authors <github@kotlinnlp.com>
2017-06-20 16:31:06 +0200

    Rename to ParametersUnit

--- src/kotlin/com/kotlinnlp/simplednn/core/layers/ParametersUnit.kt
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/ParametersUnit.kt
@@ -5,7 +5,7 @@
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package com.kotlinnlp.simplednn.core.layers.recurrent
+package com.kotlinnlp.simplednn.core.layers
 
 import com.kotlinnlp.simplednn.core.arrays.UpdatableArray
 import com.kotlinnlp.simplednn.core.arrays.UpdatableDenseArray
@@ -15,7 +15,7 @@ import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
 /**
  *
  */
-data class GateParametersUnit(val inputSize: Int, val outputSize: Int, private val sparseInput: Boolean = false) {
+data class ParametersUnit(val inputSize: Int, val outputSize: Int, private val sparseInput: Boolean = false) {
 
   /**
    *
