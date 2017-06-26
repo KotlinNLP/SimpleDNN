b734d6095f59363fbdc19a9cae33a9ca46123d37
KotlinNLP Authors <github@kotlinnlp.com>
2017-06-26 17:04:11 +0200

    Add GatedRecurrentLayerStructure

---
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/recurrent/GatedRecurrentLayerStructure.kt
@@ -0,0 +1,53 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.layers.recurrent
+
+import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.layers.*
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+
+/**
+ * The Recurrent Layer Structure.
+ *
+ * @property inputArray the input array of the layer
+ * @property outputArray the output array of the layer
+ * @property params the parameters which connect the input to the output
+ * @property layerContextWindow the context window used for the forward and the backward
+ * @property activationFunction the activation function of the layer
+ * @property dropout the probability of dropout (default 0.0).
+ *                   If applying it, the usual value is 0.5 (better 0.25 if it's the first layer).
+ */
+abstract class GatedRecurrentLayerStructure<InputNDArrayType : NDArray<InputNDArrayType>>(
+  inputArray: AugmentedArray<InputNDArrayType>,
+  outputArray: AugmentedArray<DenseNDArray>,
+  params: LayerParameters,
+  layerContextWindow: LayerContextWindow,
+  activationFunction: ActivationFunction? = null,
+  dropout: Double = 0.0
+) : RecurrentLayerStructure<InputNDArrayType>(
+  inputArray = inputArray,
+  outputArray = outputArray,
+  params = params,
+  layerContextWindow = layerContextWindow,
+  activationFunction = activationFunction,
+  dropout = dropout) {
+
+  /**
+   * The helper which calculates the relevance
+   */
+  override abstract val relevanceHelper: GatedRecurrentRelevanceHelper<InputNDArrayType>
+
+  /**
+   * @param layerContributions the contributions saved during the last forward
+   */
+  fun propagateRelevanceToGates(layerContributions: LayerParameters) {
+    this.relevanceHelper.propagateRelevanceToGates(layerContributions)
+  }
+}
