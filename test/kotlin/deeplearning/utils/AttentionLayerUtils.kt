dabcef43bb6fbefb5360029bfaa03b9a6a520529
KotlinNLP Authors <github@kotlinnlp.com>
2017-07-05 16:53:06 +0200

    Add methods

--- test/kotlin/deeplearning/utils/AttentionLayerUtils.kt
+++ test/kotlin/deeplearning/utils/AttentionLayerUtils.kt
@@ -13,6 +13,7 @@ import com.kotlinnlp.simplednn.core.layers.LayerStructureFactory
 import com.kotlinnlp.simplednn.core.layers.LayerType
 import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerParameters
 import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerStructure
+import com.kotlinnlp.simplednn.deeplearning.attentionnetwork.AttentionNetworkParameters
 import com.kotlinnlp.simplednn.deeplearning.attentionnetwork.attentionlayer.AttentionLayerParameters
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
@@ -101,6 +102,40 @@ object AttentionLayerUtils {
     params.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(
       doubleArrayOf(-0.9, 0.1)
     ))
+
+    return params
+  }
+
+  /**
+   *
+   */
+  fun buildAttentionNetworkParams1(): AttentionNetworkParameters {
+
+    val params = AttentionNetworkParameters(inputSize = 4, attentionSize = 2, sparseInput = false)
+    val transformParams = this.buildTransformLayerParams1()
+
+    params.transformParams.unit.weights.values.assignValues(transformParams.unit.weights.values)
+    params.transformParams.unit.biases.values.assignValues(transformParams.unit.biases.values)
+    params.attentionParams.contextVector.values.assignValues(
+      DenseNDArrayFactory.arrayOf(doubleArrayOf(0.2, -0.6))
+    )
+
+    return params
+  }
+
+  /**
+   *
+   */
+  fun buildAttentionNetworkParams2(): AttentionNetworkParameters {
+
+    val params = AttentionNetworkParameters(inputSize = 4, attentionSize = 2, sparseInput = false)
+    val transformParams = this.buildTransformLayerParams2()
+
+    params.transformParams.unit.weights.values.assignValues(transformParams.unit.weights.values)
+    params.transformParams.unit.biases.values.assignValues(transformParams.unit.biases.values)
+    params.attentionParams.contextVector.values.assignValues(
+      DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.1, 0.4))
+    )
 
     return params
   }
