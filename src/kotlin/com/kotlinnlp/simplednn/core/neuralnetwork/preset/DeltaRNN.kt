af404b1ebb62a670ef5745e1c47129f44bff282a
KotlinNLP Authors <github@kotlinnlp.com>
2017-07-12 13:09:47 +0200

    Add DeltaRNN preset

---
+++ src/kotlin/com/kotlinnlp/simplednn/core/neuralnetwork/preset/DeltaRNN.kt
@@ -0,0 +1,42 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.neuralnetwork.preset
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.layers.LayerConfiguration
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.core.neuralnetwork.NeuralNetwork
+
+/**
+ * The Delta Recurrent Neural Network factory.
+ */
+object DeltaRNN {
+
+  operator fun invoke(inputSize: Int,
+                      hiddenSize: Int,
+                      hiddenActivation: ActivationFunction?,
+                      outputSize: Int,
+                      outputActivation: ActivationFunction?,
+                      inputType: LayerType.Input = LayerType.Input.Dense,
+                      dropout: Double = 0.0) = NeuralNetwork(
+    LayerConfiguration(
+      size = inputSize,
+      inputType = inputType
+    ),
+    LayerConfiguration(
+      size = hiddenSize,
+      activationFunction = hiddenActivation,
+      connectionType = LayerType.Connection.DeltaRNN,
+      dropout = dropout
+    ),
+    LayerConfiguration(
+      size = outputSize,
+      activationFunction = outputActivation,
+      connectionType = LayerType.Connection.Feedforward)
+  )
+}
