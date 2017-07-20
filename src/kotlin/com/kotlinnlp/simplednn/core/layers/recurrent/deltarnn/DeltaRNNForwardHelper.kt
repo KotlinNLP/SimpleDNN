682aeffd13b2f96f969cab26e0d8e90688853b7e
KotlinNLP Authors <github@kotlinnlp.com>
2017-07-20 18:53:34 +0200

    Implement forward with relevance

--- src/kotlin/com/kotlinnlp/simplednn/core/layers/recurrent/deltarnn/DeltaRNNForwardHelper.kt
+++ src/kotlin/com/kotlinnlp/simplednn/core/layers/recurrent/deltarnn/DeltaRNNForwardHelper.kt
@@ -37,7 +37,7 @@ class DeltaRNNForwardHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
 
     wx.assignDot(w, x)
 
-    val c: DenseNDArray = this.calculateCandidate(yPrev = yPrev)
+    val c: DenseNDArray = this.calculateCandidate(wyRec = this.calculateRecurrentContribution(yPrev))
     val p: DenseNDArray = this.calculatePartition()
 
     val y: DenseNDArray = this.layer.outputArray.values
@@ -56,6 +56,42 @@ class DeltaRNNForwardHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
    * @param layerContributions the structure in which to save the contributions during the calculations
    */
   override fun forward(layerContributions: LayerParameters) {
+    this.layer.params as DeltaRNNLayerParameters
+    layerContributions as DeltaRNNLayerParameters
+
+    val wx: DenseNDArray = this.layer.wx.values
+    var wyRec: DenseNDArray? = null
+    val prevStateLayer: LayerStructure<*>? = this.layer.layerContextWindow.getPrevStateLayer()
+    val yPrev: DenseNDArray? = prevStateLayer?.outputArray?.values
+
+    // w (dot) x
+    this.forwardArray(
+      contributions = layerContributions.feedforwardUnit.weights.values,
+      x = this.layer.inputArray.values,
+      y = wx,
+      w = this.layer.params.feedforwardUnit.weights.values as DenseNDArray)
+
+    // wRec (dot) yPrev
+    if (yPrev != null) {
+      wyRec = this.layer.wyRec.values
+      this.forwardArray(
+        contributions = layerContributions.recurrentUnit.weights.values,
+        x =  yPrev,
+        y = wyRec,
+        w = this.layer.params.recurrentUnit.weights.values as DenseNDArray)
+    }
+
+    val c: DenseNDArray = this.calculateCandidate(wyRec = wyRec)
+    val p: DenseNDArray = this.calculatePartition()
+
+    val y: DenseNDArray = this.layer.outputArray.values
+    y.assignProd(p, c)
+
+    if (yPrev != null) {
+      y.assignSum(p.reverseSub(1.0).assignProd(yPrev))
+    }
+
+    this.layer.outputArray.activate()
   }
 
   /**
@@ -85,18 +121,17 @@ class DeltaRNNForwardHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
    *   d2 = alpha * w (dot) x * wRec (dot) yPrev
    *   c = tanh(d1 + d2 + bc)
    *
-   * @param yPrev the output array in the previous state
+   * @param wyRec the result of the dot product between wRec and yPrev if any, null otherwise
    *
    * @return the array of candidate
    */
-  private fun calculateCandidate(yPrev: DenseNDArray?): DenseNDArray {
+  private fun calculateCandidate(wyRec: DenseNDArray?): DenseNDArray {
     this.layer.params as DeltaRNNLayerParameters
 
     val c: DenseNDArray = this.layer.candidate.values
     val bc: DenseNDArray = this.layer.params.feedforwardUnit.biases.values
     val beta1: DenseNDArray = this.layer.params.beta1.values
     val wx: DenseNDArray = this.layer.wx.values
-    val wyRec: DenseNDArray? = this.calculateRecurrentContribution(yPrev)
 
     val d1: DenseNDArray = beta1.prod(wx)
 
