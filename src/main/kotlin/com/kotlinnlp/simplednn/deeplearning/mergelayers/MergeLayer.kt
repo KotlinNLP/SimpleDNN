00305960e7f8a7e4bb768c11a305ef343ff468ce
KotlinNLP Authors <github@kotlinnlp.com>
2017-09-01 23:25:14 +0200

    Add MergeLayer

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/MergeLayer.kt
@@ -0,0 +1,34 @@
+package com.kotlinnlp.simplednn.deeplearning.mergelayers
+
+import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.layers.LayerParameters
+import com.kotlinnlp.simplednn.core.layers.LayerStructure
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+
+/**
+ * The Merge Layer abstract class.
+ * It is a [LayerStructure] with two inputs instead of one.
+ *
+ * @property inputArray1 the first input array of the layer
+ * @property inputArray2 the second input array of the layer
+ * @property outputArray the output array of the layer
+ * @property params the parameters which connect the input to the output
+ * @property activationFunction the activation function of the layer
+ * @property dropout the probability of dropout (default 0.0).
+ *                   If applying it, the usual value is 0.5 (better 0.25 if it's the first layer).
+ */
+abstract class MergeLayer<InputNDArrayType : NDArray<InputNDArrayType>>(
+  val inputArray1: AugmentedArray<InputNDArrayType>,
+  val inputArray2: AugmentedArray<InputNDArrayType>,
+  outputArray: AugmentedArray<DenseNDArray>,
+  params: LayerParameters,
+  activationFunction: ActivationFunction? = null,
+  dropout: Double = 0.0
+) : LayerStructure<InputNDArrayType>(
+  inputArray = inputArray1,
+  outputArray = outputArray,
+  params = params,
+  activationFunction = activationFunction,
+  dropout = dropout)
