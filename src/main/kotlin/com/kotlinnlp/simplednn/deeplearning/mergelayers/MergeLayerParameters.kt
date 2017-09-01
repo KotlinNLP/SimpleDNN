76d26ee1589e2c4cf5e85dba4b991f66599136d2
KotlinNLP Authors <github@kotlinnlp.com>
2017-09-01 23:52:53 +0200

    Add header

--- src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/MergeLayerParameters.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/MergeLayerParameters.kt
@@ -1,3 +1,10 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
 package com.kotlinnlp.simplednn.deeplearning.mergelayers
 
 import com.kotlinnlp.simplednn.core.layers.LayerParameters
