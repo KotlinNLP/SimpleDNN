fe3fdcdb74e5c286c44a12edf1bccc412593f532
KotlinNLP Authors <github@kotlinnlp.com>
2017-09-01 23:52:34 +0200

    Replace LayerParameters with MergeLayerParameters

--- src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/MergeLayer.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/MergeLayer.kt
@@ -1,8 +1,14 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
 package com.kotlinnlp.simplednn.deeplearning.mergelayers
 
 import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
 import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
-import com.kotlinnlp.simplednn.core.layers.LayerParameters
 import com.kotlinnlp.simplednn.core.layers.LayerStructure
 import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
@@ -23,7 +29,7 @@ abstract class MergeLayer<InputNDArrayType : NDArray<InputNDArrayType>>(
   val inputArray1: AugmentedArray<InputNDArrayType>,
   val inputArray2: AugmentedArray<InputNDArrayType>,
   outputArray: AugmentedArray<DenseNDArray>,
-  params: LayerParameters,
+  params: MergeLayerParameters,
   activationFunction: ActivationFunction? = null,
   dropout: Double = 0.0
 ) : LayerStructure<InputNDArrayType>(
@@ -34,7 +40,7 @@ abstract class MergeLayer<InputNDArrayType : NDArray<InputNDArrayType>>(
   dropout = dropout) {
 
   /**
-   * @return the [LayerParameters] used to store errors, compatible with this type of [MergeLayer]
+   * @return the [MergeLayerParameters] used to store errors, compatible with this type of [MergeLayer]
    */
-  abstract fun parametersErrorsFactory(): LayerParameters
+  abstract fun parametersErrorsFactory(): MergeLayerParameters
 }
