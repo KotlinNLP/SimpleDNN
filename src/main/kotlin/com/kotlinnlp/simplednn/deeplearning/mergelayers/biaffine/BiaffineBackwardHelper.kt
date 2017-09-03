ab8ac571e0b9504bb96970d0090e9676f61a66be
KotlinNLP Authors <github@kotlinnlp.com>
2017-09-03 18:44:51 +0200

    Implement backward

--- src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/biaffine/BiaffineBackwardHelper.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/biaffine/BiaffineBackwardHelper.kt
@@ -7,9 +7,13 @@
 
 package com.kotlinnlp.simplednn.deeplearning.mergelayers.biaffine
 
+import com.kotlinnlp.simplednn.core.arrays.UpdatableArray
 import com.kotlinnlp.simplednn.core.layers.BackwardHelper
 import com.kotlinnlp.simplednn.core.layers.LayerParameters
-import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.*
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import com.kotlinnlp.simplednn.simplemath.ndarray.sparsebinary.SparseBinaryNDArray
 
 /**
  * The helper which executes the backward on a biaffine [layer].
@@ -38,22 +42,104 @@ class BiaffineBackwardHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
 
     this.layer.applyOutputActivationDeriv()
 
-    this.assignParamsGradients()
+    val gwx: Array<DenseNDArray> = this.getWXArraysGradients()
+
+    this.assignParamsGradients(wxErrors = gwx)
 
     if (propagateToInput) {
-      this.assignLayerGradients()
+      this.assignLayerGradients(wxErrors = gwx)
     }
   }
 
   /**
+   *
    */
-  private fun assignParamsGradients() {
-    TODO("not implemented")
+  private fun getWXArraysGradients(): Array<DenseNDArray> {
+    // TODO: actually the wx errors are Sparse if the input is SparseBinary: calculations should be optimized
+
+    val x2: InputNDArrayType = this.layer.inputArray2.values
+    val gy: DenseNDArray = this.layer.outputArray.errors
+
+    return Array(
+      size = this.layer.params.outputSize,
+      init = { i ->
+        val gwi: Double = gy[i]
+
+        when (x2) {
+          is DenseNDArray -> x2.prod(gwi)
+          is SparseBinaryNDArray -> {
+            // TODO: actually the wx arrays are sparse, replace with the commented code to optimize further calculations
+            val tmpArray = DenseNDArrayFactory.zeros(shape = x2.shape)
+            val mask: NDArrayMask = x2.mask
+
+            for (k in 0 until mask.dim1.size) {
+              tmpArray[mask.dim1[k], mask.dim2[k]] = gwi
+            }
+
+            tmpArray
+
+//            val zeros = DenseNDArrayFactory.zeros(shape = x2.shape)
+//            val mask: NDArrayMask = x2.mask
+//            SparseNDArrayFactory.arrayOf(
+//              activeIndicesValues = Array(
+//                size = mask.dim1.size,
+//                init = { k -> SparseEntry(Indices(mask.dim1[k], mask.dim2[k]), gwi) }
+//              ),
+//              shape = x2.shape
+//            )
+          }
+          else -> throw RuntimeException("Invalid input type")
+        }
+      }
+    )
   }
 
   /**
+   *
    */
-  private fun assignLayerGradients() {
-    TODO("not implemented")
+  private fun assignParamsGradients(wxErrors: Array<DenseNDArray>) {
+    // TODO: actually the wx errors are Sparse if the input is SparseBinary: calculations should be optimized
+
+    val x1: InputNDArrayType = this.layer.inputArray.values
+    val x2: InputNDArrayType = this.layer.inputArray2.values
+
+    val gy: DenseNDArray = this.layer.outputArray.errors
+    val gwArrays: Array<UpdatableArray<*>> = this.paramsErrors.w
+    val gw1: NDArray<*> = this.paramsErrors.w1.values
+    val gw2: NDArray<*> = this.paramsErrors.w2.values
+    val gb: NDArray<*> = this.paramsErrors.b.values
+
+    gwArrays.forEachIndexed { i, gwArray ->
+      val gwi: DenseNDArray = gwArray.values as DenseNDArray
+      val gwxi: DenseNDArray = wxErrors[i]
+      gwi.assignDot(gwxi, x1.T)
+    }
+
+    gw1.assignDot(gy, x1.T)
+    gw2.assignDot(gy, x2.T)
+    gb.assignValues(gy)
+  }
+
+  /**
+   *
+   */
+  private fun assignLayerGradients(wxErrors: Array<DenseNDArray>) {
+    // TODO: actually the wx errors are Sparse if the input is SparseBinary: calculations should be optimized
+
+    val w1: DenseNDArray = this.layer.params.w1.values as DenseNDArray
+    val w2: DenseNDArray = this.layer.params.w2.values as DenseNDArray
+    val wArrays: Array<UpdatableArray<*>> = this.layer.params.w
+
+    val gy: DenseNDArray = this.layer.outputArray.errors
+    val gyT: DenseNDArray = gy.T
+
+    this.layer.inputArray1.assignErrors(w1.dot(gyT))
+    this.layer.inputArray2.assignErrors(w2.dot(gyT))
+
+    wxErrors.forEachIndexed { i, wxError ->
+      val wi: DenseNDArray = wArrays[i].values as DenseNDArray
+      this.layer.inputArray1.errors.assignSum(wi.dot(wxError))
+      this.layer.inputArray2.errors.assignSum(wxError.prod(gy[i]))
+    }
   }
 }
