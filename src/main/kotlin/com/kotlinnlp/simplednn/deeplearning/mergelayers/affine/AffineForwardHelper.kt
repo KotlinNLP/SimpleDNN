1af02832638543f75ffa147bfbc3e75e8f2b5515
KotlinNLP Authors <github@kotlinnlp.com>
2017-09-05 14:50:17 +0200

    Fix output values assignment

--- src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/affine/AffineForwardHelper.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/mergelayers/affine/AffineForwardHelper.kt
@@ -30,13 +30,12 @@ class AffineForwardHelper<InputNDArrayType : NDArray<InputNDArrayType>>(
 
     val x1: InputNDArrayType = this.layer.inputArray.values
     val x2: InputNDArrayType = this.layer.inputArray2.values
-    val y: DenseNDArray = this.layer.outputArray.values
 
     val w1: DenseNDArray = this.layer.params.w1.values as DenseNDArray
     val w2: DenseNDArray = this.layer.params.w2.values as DenseNDArray
     val b: DenseNDArray = this.layer.params.b.values
 
-    y.assignDot(w1, x1).assignSum(w2.dot(x2)).assignSum(b)
+    this.layer.outputArray.assignValues(w1.dot(x1).assignSum(w2.dot(x2)).assignSum(b))
 
     this.layer.outputArray.activate()
   }
