2e9c3c1d0af589260c57e6f445e9dd4da00f046d
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-01 17:05:06 +0100

    Add DictionarySet

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/utils/DictionarySet.kt
@@ -0,0 +1,92 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.utils
+
+import com.google.common.collect.BiMap
+import com.google.common.collect.HashBiMap
+import com.google.common.collect.HashMultiset
+import com.google.common.collect.Multisets
+
+/**
+ * A dictionary containing a set of elements.
+ * Elements are mapped bi-univocally to ids.
+ * It provides methods to get information about elements, like their occurrences count and frequency.
+ */
+class DictionarySet<T>{
+
+  /**
+   * The number of distinct element of this set.
+   */
+  val size: Int get() = this.elementsMultiset.elementSet().size
+
+  /**
+   * The elements multiset with adding properties (e.g. the count of insertions of an element).
+   */
+  private val elementsMultiset: HashMultiset<T> = HashMultiset.create()
+
+  /**
+   * The [BiMap] of elements to ids.
+   */
+  private val elementsBiMap: BiMap<T, Int> = HashBiMap.create()
+
+  /**
+   * Add the given [element] to the dictionary, incrementing the count of its occurrences.
+   *
+   * @param element the element to add
+   */
+  fun add(element: T) {
+
+    this.elementsMultiset.add(element)
+
+    if (element !in this.elementsBiMap) {
+      this.elementsBiMap.put(key = element, value = this.elementsMultiset.elementSet().size - 1)
+    }
+  }
+
+  /**
+   * Get the element associated to the given [id] if it exists, null otherwise.
+   *
+   * @param id the id of an element
+   *
+   * @return the element with the given [id] or null
+   */
+  fun getElement(id: Int): T? = this.elementsBiMap.inverse()[id]
+
+  /**
+   * @param element an element
+   *
+   * @return the id of the given [element] if it is present in the dictionary, null otherwise
+   */
+  fun getId(element: T): Int? = this.elementsBiMap[element]
+
+  /**
+   * @param element an element
+   *
+   * @return the occurrences count of the given [element] (0 if it is not present)
+   */
+  fun getCount(element: T): Int = this.elementsMultiset.count(element)
+
+  /**
+   * @param id the id of an element
+   *
+   * @return the occurrences count of the element with the given [id] (0 if it is not present)
+   */
+  fun getCount(id: Int): Int = this.elementsMultiset.count(this.getElement(id))
+
+  /**
+   * @param id the id of an element
+   *
+   * @return the count of the element with the given [id]  (0 if it is not present)
+   */
+  fun getFrequency(id: Int): Int = this.elementsMultiset.count(this.getElement(id)) / this.size
+
+  /**
+   * @return a set of the elements in the dictionary, sorted by descending order of occurrences
+   */
+  fun getSortedElements(): Set<T> = Multisets.copyHighestCountFirst(this.elementsMultiset).elementSet()
+}
