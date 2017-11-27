f1ca661e6a5639078c72bae7ea77406e528915c5
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-27 18:32:09 +0100

    Add 'mePropK' parameter to the backward and add getOutputMask() method

--- src/main/kotlin/com/kotlinnlp/simplednn/core/layers/BackwardHelper.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/layers/BackwardHelper.kt
@@ -8,6 +8,9 @@
 package com.kotlinnlp.simplednn.core.layers
 
 import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArrayMask
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import java.util.*
 
 /**
  * The helper which executes the backward on a [layer].
@@ -25,6 +28,30 @@ interface BackwardHelper<InputNDArrayType : NDArray<InputNDArrayType>> {
    *
    * @param paramsErrors the errors of the parameters which will be filled
    * @param propagateToInput whether to propagate the errors to the input array
+   * @param mePropK the k factor of the 'meProp' algorithm to propagate from top k (in percentage) output nodes
+   *                (ignored if null)
    */
-  fun backward(paramsErrors: LayerParameters<*>, propagateToInput: Boolean = false)
+  fun backward(paramsErrors: LayerParameters<*>, propagateToInput: Boolean = false, mePropK: Double?)
+
+  /**
+   * @param mePropK the k factor of the 'meProp' algorithm (the percentage of the top k nodes)
+   *
+   * @return the mask of the top k ([mePropK]) elements of the output array.
+   */
+  fun getOutputMask(mePropK: Double): NDArrayMask {
+
+    val y: DenseNDArray = this.layer.outputArray.values
+    val nTopElements: Int = Math.round(mePropK * y.length).toInt()
+
+    val minHeap = PriorityQueue<Pair<Int, Double>>(y.length, Comparator({ a, b ->
+      val diff: Double = a.second - b.second
+      if (diff > 0.0) -1 else if (diff < 0.0) 1 else 0
+    }))
+
+    (0 until y.length).forEach { i -> minHeap.add(Pair(i, y[i])) }
+
+    return NDArrayMask(
+      dim1 = Array(size = nTopElements, init = { minHeap.remove().first }),
+      dim2 = Array(size = nTopElements, init = { 0 }))
+  }
 }
