36afdf90e2dac4b3043afb863105cbd91efecea9
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-28 17:16:59 +0100

    Add method tests

--- src/test/kotlin/ndarray/SparseNDArraySpec.kt
+++ src/test/kotlin/ndarray/SparseNDArraySpec.kt
@@ -8,6 +8,7 @@
 package ndarray
 
 import com.kotlinnlp.simplednn.simplemath.ndarray.Indices
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArrayMask
 import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
 import com.kotlinnlp.simplednn.simplemath.ndarray.SparseEntry
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
@@ -174,6 +175,105 @@ class SparseNDArraySpec : Spek({
 
         it("should assign the expected values") {
           assertTrue { res.equals(expectedArray, tolerance = 1.0e-04) }
+        }
+      }
+    }
+
+    context("math methods in-place") {
+
+      on ("assignValues(denseArray, mask) method") {
+
+        val aS = SparseNDArrayFactory.arrayOf(activeIndicesValues = arrayOf(), shape = Shape(4, 3))
+        val aD = DenseNDArrayFactory.arrayOf(arrayOf(
+          doubleArrayOf(0.2, 0.9, 0.4),
+          doubleArrayOf(0.5, 0.6, 0.1),
+          doubleArrayOf(0.3, 0.4, 0.6),
+          doubleArrayOf(0.1, 0.0, 0.1)
+        ))
+        val bD = DenseNDArrayFactory.arrayOf(arrayOf(
+          doubleArrayOf(0.2, 0.9, 0.4),
+          doubleArrayOf(0.5, 0.6, 0.1)
+        ))
+        val mask = NDArrayMask(dim1 = arrayOf(0, 1, 1, 3), dim2 = arrayOf(1, 0, 2, 2))
+
+        val expectedArray = SparseNDArrayFactory.arrayOf(
+          activeIndicesValues = arrayOf(
+            SparseEntry(Indices(0, 1), 0.9),
+            SparseEntry(Indices(1, 0), 0.5),
+            SparseEntry(Indices(1, 2), 0.1),
+            SparseEntry(Indices(3, 2), 0.1)
+          ),
+          shape = Shape(4, 3))
+
+        val res = aS.assignValues(aD, mask = mask)
+
+        it("should return the same DenseNDArray") {
+          assertTrue { aS === res }
+        }
+
+        it("should assign the expected values") {
+          assertTrue { res.equals(expectedArray, tolerance = 1.0e-04) }
+        }
+
+        it("should throw an error with not compatible shapes") {
+          assertFails { aS.assignValues(bD, mask = mask) }
+        }
+      }
+
+      on ("assignDot(sparseArray, denseArray) method") {
+
+        val a = SparseNDArrayFactory.arrayOf(activeIndicesValues = arrayOf(), shape = Shape(4, 3))
+
+        val aS = SparseNDArrayFactory.arrayOf(
+          activeIndicesValues = arrayOf(
+            SparseEntry(Indices(0, 0), 0.5),
+            SparseEntry(Indices(2, 0), 1.0)
+          ),
+          shape = Shape(4))
+
+        val mS = SparseNDArrayFactory.arrayOf(
+          activeIndicesValues = arrayOf(
+            SparseEntry(Indices(0, 0), 0.5),
+            SparseEntry(Indices(1, 1), 1.0)
+          ),
+          shape = Shape(4, 2))
+
+        val aD = DenseNDArrayFactory.arrayOf(arrayOf(
+          doubleArrayOf(0.2, 0.9, 0.5)
+        ))
+
+        val b = DenseNDArrayFactory.arrayOf(arrayOf(
+          doubleArrayOf(0.2, 0.9, 0.4),
+          doubleArrayOf(0.5, 0.6, 0.1)
+        ))
+
+        val expectedArray = SparseNDArrayFactory.arrayOf(
+          activeIndicesValues = arrayOf(
+            SparseEntry(Indices(0, 0), 0.1),
+            SparseEntry(Indices(0, 1), 0.45),
+            SparseEntry(Indices(0, 2), 0.25),
+            SparseEntry(Indices(2, 0), 0.2),
+            SparseEntry(Indices(2, 1), 0.9),
+            SparseEntry(Indices(2, 2), 0.5)
+          ),
+          shape = Shape(4, 3))
+
+        val res = a.assignDot(aS, aD)
+
+        it("should return the same DenseNDArray") {
+          assertTrue { a === res }
+        }
+
+        it("should assign the expected values") {
+          assertTrue { res.equals(expectedArray, tolerance = 1.0e-04) }
+        }
+
+        it("should throw an error with not compatible shapes") {
+          assertFails { a.assignDot(aS, b) }
+        }
+
+        it("should throw an error with matrices") {
+          assertFails { a.assignDot(mS, b) }
         }
       }
     }
