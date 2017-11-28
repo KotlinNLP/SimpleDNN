6605ae151af72ed322854b69a657d43b9ef74e52
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-28 15:41:37 +0100

    Sort mask indices

--- src/main/kotlin/com/kotlinnlp/simplednn/core/layers/BackwardHelper.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/layers/BackwardHelper.kt
@@ -52,7 +52,7 @@ interface BackwardHelper<InputNDArrayType : NDArray<InputNDArrayType>> {
     (0 until gy.length).forEach { i -> minHeap.add(Pair(i, Math.abs(gy[i]))) } // pairs of <index, value>
 
     return NDArrayMask(
-      dim1 = Array(size = nTopElements, init = { minHeap.remove().first }), // get top errors indices
+      dim1 = Array(size = nTopElements, init = { minHeap.remove().first }).sortedArray(), // get top errors indices
       dim2 = Array(size = nTopElements, init = { 0 }))
   }
 }
