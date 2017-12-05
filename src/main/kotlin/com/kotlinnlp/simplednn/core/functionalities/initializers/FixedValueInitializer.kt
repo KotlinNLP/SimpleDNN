8756b7dc409d64188566e11b8c68033bcf0ac814
KotlinNLP Authors <github@kotlinnlp.com>
2017-12-05 14:04:47 +0100

    Add FixedValueInitializer

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/initializers/FixedValueInitializer.kt
@@ -0,0 +1,27 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.functionalities.initializers
+
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+
+/**
+ * An initializer of dense arrays with the same value for all the elements.
+ *
+ * @param value the initializing value
+ */
+class FixedValueInitializer(val value: Double) : Initializer {
+
+  /**
+   * Initialize the values of the given [array].
+   *
+   * @param array a dense array
+   */
+  override fun initialize(array: DenseNDArray) {
+    array.assignValues(this.value)
+  }
+}
