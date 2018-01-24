363a13ed56b88a0617f1d10ccc961b80ece6e5ff
KotlinNLP Authors <github@kotlinnlp.com>
2018-01-24 12:43:36 +0100

    Add RecurrentAttentiveNetwork tests

---
+++ src/test/kotlin/deeplearning/recurrentattentivenetwork/RecurrentAttentiveNetworkSpec.kt
@@ -0,0 +1,205 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package deeplearning.recurrentattentivenetwork
+
+import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerParameters
+import com.kotlinnlp.simplednn.core.layers.recurrent.simple.SimpleRecurrentLayerParameters
+import com.kotlinnlp.simplednn.deeplearning.recurrentattentivedecoder.RecurrentAttentiveNetwork
+import com.kotlinnlp.simplednn.deeplearning.recurrentattentivedecoder.RecurrentAttentiveNetworkParameters
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import deeplearning.recurrentattentivenetwork.utils.RecurrentAttentiveNetworkUtils
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertTrue
+
+/**
+ *
+ */
+class RecurrentAttentiveNetworkSpec : Spek({
+
+  describe("a RecurrentAttentiveNetwork") {
+
+    val utils = RecurrentAttentiveNetworkUtils
+
+    on("forward") {
+
+      val network = RecurrentAttentiveNetwork(model = utils.buildModel())
+      val predictionLabels: List<DenseNDArray?> = utils.buildPredictionLabels()
+
+      network.setInputSequence(utils.buildInputSequence1())
+
+      val output0 = network.forward(lastPredictionLabel = predictionLabels[0], trainingMode = false)
+      val output1 = network.forward(lastPredictionLabel = predictionLabels[1], trainingMode = false)
+      val output2 = network.forward(lastPredictionLabel = predictionLabels[2], trainingMode = false)
+
+      val expectedOutputs: List<DenseNDArray> = utils.buildExpectedOutputs1()
+
+      it("should match the expected output at the forward n. 1") {
+        assertTrue { output0.equals(expectedOutputs[0], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected output at the forward n. 2") {
+        assertTrue { output1.equals(expectedOutputs[1], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected output at the forward n. 3") {
+        assertTrue { output2.equals(expectedOutputs[2], tolerance = 1.0e-06) }
+      }
+    }
+
+    on("forward with a second sequence") {
+
+      val network = RecurrentAttentiveNetwork(model = utils.buildModel())
+      val predictionLabels: List<DenseNDArray?> = utils.buildPredictionLabels()
+
+      network.setInputSequence(utils.buildInputSequence1())
+
+      network.forward(lastPredictionLabel = predictionLabels[0], trainingMode = false)
+      network.forward(lastPredictionLabel = predictionLabels[1], trainingMode = false)
+      network.forward(lastPredictionLabel = predictionLabels[2], trainingMode = false)
+
+      network.setInputSequence(utils.buildInputSequence2())
+
+      val output0 = network.forward(lastPredictionLabel = predictionLabels[0], trainingMode = false)
+      val output1 = network.forward(lastPredictionLabel = predictionLabels[1], trainingMode = false)
+      val output2 = network.forward(lastPredictionLabel = predictionLabels[2], trainingMode = false)
+
+      val expectedOutputs: List<DenseNDArray> = utils.buildExpectedOutputs2()
+
+      it("should match the expected output at the forward n. 1") {
+        assertTrue { output0.equals(expectedOutputs[0], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected output at the forward n. 2") {
+        assertTrue { output1.equals(expectedOutputs[1], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected output at the forward n. 3") {
+        assertTrue { output2.equals(expectedOutputs[2], tolerance = 1.0e-06) }
+      }
+    }
+
+    on("backward") {
+
+      val network = RecurrentAttentiveNetwork(model = utils.buildModel())
+      val predictionLabels: List<DenseNDArray?> = utils.buildPredictionLabels()
+
+      network.setInputSequence(utils.buildInputSequence1())
+
+      network.forward(lastPredictionLabel = predictionLabels[0], trainingMode = true)
+      network.forward(lastPredictionLabel = predictionLabels[1], trainingMode = true)
+      network.forward(lastPredictionLabel = predictionLabels[2], trainingMode = true)
+
+      network.backward(outputErrors = utils.getOutputErrors1())
+
+      val paramsErrors: RecurrentAttentiveNetworkParameters = network.getParamsErrors()
+      val inputSequenceErrors: List<DenseNDArray> = network.getInputSequenceErrors()
+      val contextLabelsErrors: List<DenseNDArray> = network.getContextLabelsErrors()
+
+      val expectedParamsErrors: RecurrentAttentiveNetworkParameters = utils.getExpectedParamsErrors1()
+      val expectedInputErrors: List<DenseNDArray> = utils.getExpectedInputErrors1()
+      val expectedLabelsErrors: List<DenseNDArray> = utils.getExpectedLabelsErrors1()
+
+      it("should match the expected errors of the transform layer weights") {
+        assertTrue {
+          paramsErrors.transformParams.unit.weights.values.equals(
+            expectedParamsErrors.transformParams.unit.weights.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the transform layer biases") {
+        assertTrue {
+          paramsErrors.transformParams.unit.biases.values.equals(
+            expectedParamsErrors.transformParams.unit.biases.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the context vector") {
+        assertTrue {
+          paramsErrors.attentionParams.attentionParams.contextVector.values.equals(
+            expectedParamsErrors.attentionParams.attentionParams.contextVector.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the recurrent context network weights") {
+        assertTrue {
+          (paramsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+            .unit.weights.values.equals(
+            (expectedParamsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+              .unit.weights.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the recurrent context network biases") {
+        assertTrue {
+          (paramsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+            .unit.biases.values.equals(
+            (expectedParamsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+              .unit.biases.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the recurrent context network recurrent weights") {
+        assertTrue {
+          (paramsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+            .unit.recurrentWeights.values.equals(
+            (expectedParamsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters)
+              .unit.recurrentWeights.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the output network weights") {
+        assertTrue {
+          (paramsErrors.outputParams.paramsPerLayer[0] as FeedforwardLayerParameters).unit.weights.values.equals(
+            (expectedParamsErrors.outputParams.paramsPerLayer[0] as FeedforwardLayerParameters).unit.weights.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the output network biases") {
+        assertTrue {
+          (paramsErrors.outputParams.paramsPerLayer[0] as FeedforwardLayerParameters).unit.biases.values.equals(
+            (expectedParamsErrors.outputParams.paramsPerLayer[0] as FeedforwardLayerParameters).unit.biases.values,
+            tolerance = 1.0e-06)
+        }
+      }
+
+      it("should match the expected errors of the first context label") {
+        assertTrue { contextLabelsErrors[0].equals(expectedLabelsErrors[0], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected errors of the second context label") {
+        assertTrue { contextLabelsErrors[1].equals(expectedLabelsErrors[1], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected errors of the third context label") {
+        assertTrue { contextLabelsErrors[2].equals(expectedLabelsErrors[2], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected errors of the first array of the sequence") {
+        assertTrue { inputSequenceErrors[0].equals(expectedInputErrors[0], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected errors of the second array of the sequence") {
+        assertTrue { inputSequenceErrors[1].equals(expectedInputErrors[1], tolerance = 1.0e-06) }
+      }
+
+      it("should match the expected errors of the third array of the sequence") {
+        assertTrue { inputSequenceErrors[2].equals(expectedInputErrors[2], tolerance = 1.0e-06) }
+      }
+    }
+  }
+})
---
+++ src/test/kotlin/deeplearning/recurrentattentivenetwork/utils/RecurrentAttentiveNetworkUtils.kt
@@ -0,0 +1,261 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package deeplearning.recurrentattentivenetwork.utils
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.Tanh
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerParameters
+import com.kotlinnlp.simplednn.core.layers.recurrent.simple.SimpleRecurrentLayerParameters
+import com.kotlinnlp.simplednn.deeplearning.recurrentattentivedecoder.RecurrentAttentiveNetworkModel
+import com.kotlinnlp.simplednn.deeplearning.recurrentattentivedecoder.RecurrentAttentiveNetworkParameters
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+
+/**
+ *
+ */
+object RecurrentAttentiveNetworkUtils {
+
+  /**
+   *
+   */
+  fun buildModel(): RecurrentAttentiveNetworkModel {
+
+    val model = RecurrentAttentiveNetworkModel(
+      inputSize = 2,
+      attentionSize = 2,
+      recurrentContextSize = 2,
+      contextLabelSize = 2,
+      outputSize = 2,
+      contextActivation = Tanh(),
+      contextRecurrenceType = LayerType.Connection.SimpleRecurrent,
+      outputActivationFunction = Tanh())
+
+    this.setTransformLayerParams(model)
+    this.setAttentionNetworkParams(model)
+    this.setRecurrentContextNetworkParams(model)
+    this.setOutputNetworkParams(model)
+
+    return model
+  }
+
+  /**
+   *
+   */
+  fun buildInputSequence1(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.4, 0.7)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.5, 0.7)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.3, -0.5))
+  )
+
+  /**
+   *
+   */
+  fun buildInputSequence2(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.6, 0.4)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(1.0, 0.3)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.4, 0.1))
+  )
+
+  /**
+   *
+   */
+  fun buildPredictionLabels(): List<DenseNDArray?> = listOf(
+    null,
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.6, -0.6)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.4, 0.8))
+  )
+
+  /**
+   *
+   */
+  fun buildExpectedOutputs1(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.431531, -0.360826)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.334841, -0.853749)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.907658, -0.252679))
+  )
+
+  /**
+   *
+   */
+  fun buildExpectedOutputs2(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.058941, -0.538804)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.469632, -0.853854)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.786288, -0.468557))
+  )
+
+  /**
+   *
+   */
+  fun getExpectedInputErrors1(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.310567, -0.164335)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.319349, -0.165902)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.293844, -0.153219))
+  )
+
+  /**
+   *
+   */
+  fun getExpectedLabelsErrors1(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.464371, -0.636690)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.120753, 0.927892)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.020162, 0.012493))
+  )
+
+  /**
+   *
+   */
+  fun getExpectedParamsErrors1(): RecurrentAttentiveNetworkParameters {
+
+    val paramsErrors: RecurrentAttentiveNetworkParameters = this.buildModel().params.copy()
+
+    this.setTransformLayerParamsErrors(paramsErrors)
+    this.setAttentionNetworkParamsErrors(paramsErrors)
+    this.setRecurrentContextNetworkParamsErrors(paramsErrors)
+    this.setOutputNetworkParamsErrors(paramsErrors)
+
+    return paramsErrors
+  }
+
+  /**
+   *
+   */
+  fun getOutputErrors1(): List<DenseNDArray> = listOf(
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(0.2, 0.9)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.9, 0.0)),
+    DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.1, 0.4))
+  )
+
+  /**
+   *
+   */
+  private fun setTransformLayerParams(model: RecurrentAttentiveNetworkModel) {
+
+    val params = model.transformParams
+
+    params.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.3, 0.4, 0.8, -0.6),
+      doubleArrayOf(0.2, -0.1, -0.9, 1.0)
+    )))
+
+    params.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(
+      doubleArrayOf(0.3, -0.4)
+    ))
+  }
+
+  /**
+   *
+   */
+  private fun setAttentionNetworkParams(model: RecurrentAttentiveNetworkModel) {
+
+    val params = model.attentionParams.attentionParams
+
+    params.contextVector.values.assignValues(DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.3, -0.5)))
+  }
+
+  /**
+   *
+   */
+  private fun setRecurrentContextNetworkParams(model: RecurrentAttentiveNetworkModel) {
+
+    val params = model.recurrentContextNetwork.model.paramsPerLayer[0] as SimpleRecurrentLayerParameters
+
+    params.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.7, -0.2, -0.4, 1.0),
+      doubleArrayOf(0.0, 0.3, -0.7, -0.9)
+    )))
+
+    params.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(doubleArrayOf(0.8, 0.6)))
+
+    params.unit.recurrentWeights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(-0.9, 0.7),
+      doubleArrayOf(0.2, -0.9)
+    )))
+  }
+
+  /**
+   *
+   */
+  private fun setOutputNetworkParams(model: RecurrentAttentiveNetworkModel) {
+
+    val params = model.outputNetwork.model.paramsPerLayer[0] as FeedforwardLayerParameters
+
+    params.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.9, -0.7, -0.8, 0.7),
+      doubleArrayOf(-0.6, -0.6, 0.8, 0.1)
+    )))
+
+    params.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(
+      doubleArrayOf(0.1, -0.9)
+    ))
+  }
+
+  /**
+   *
+   */
+  private fun setTransformLayerParamsErrors(paramsErrors: RecurrentAttentiveNetworkParameters) {
+
+    val errors = paramsErrors.transformParams
+
+    errors.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.001854, -0.002859, -0.000462, -0.001199),
+      doubleArrayOf(0.002297, -0.003461, -0.000493, -0.002329)
+    )))
+
+    errors.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(
+      doubleArrayOf(-0.002562, -0.004909)
+    ))
+  }
+
+  /**
+   *
+   */
+  private fun setAttentionNetworkParamsErrors(paramsErrors: RecurrentAttentiveNetworkParameters) {
+
+    val errors = paramsErrors.attentionParams.attentionParams
+
+    errors.contextVector.values.assignValues(DenseNDArrayFactory.arrayOf(doubleArrayOf(0.005547, -0.004150)))
+  }
+
+  /**
+   *
+   */
+  private fun setRecurrentContextNetworkParamsErrors(paramsErrors: RecurrentAttentiveNetworkParameters) {
+
+    val errors = paramsErrors.recurrentContextParams.paramsPerLayer[0] as SimpleRecurrentLayerParameters
+
+    errors.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(-0.037181, 0.055854, 0.105429, -0.095281),
+      doubleArrayOf(0.031228, -0.046914, -0.090906, 0.096628)
+    )))
+
+    errors.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(doubleArrayOf(0.169808, 0.076196)))
+
+    errors.unit.recurrentWeights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.109232, 0.095000),
+      doubleArrayOf(-0.104809, -0.081011)
+    )))
+  }
+
+  /**
+   *
+   */
+  private fun setOutputNetworkParamsErrors(paramsErrors: RecurrentAttentiveNetworkParameters) {
+
+    val errors = paramsErrors.outputParams.paramsPerLayer[0] as FeedforwardLayerParameters
+
+    errors.unit.weights.values.assignValues(DenseNDArrayFactory.arrayOf(arrayOf(
+      doubleArrayOf(0.045206, -0.067858, 0.147133, -0.082301),
+      doubleArrayOf(-0.079239, 0.118969, 0.292940, 0.058410)
+    )))
+
+    errors.unit.biases.values.assignValues(DenseNDArrayFactory.arrayOf(
+      doubleArrayOf(-0.217984, 0.385762)
+    ))
+  }
+}
