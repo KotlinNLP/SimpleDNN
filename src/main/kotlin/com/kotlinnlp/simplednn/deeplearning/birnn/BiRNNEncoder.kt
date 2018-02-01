b0c1ea1c35742b57ea6b823497c0bf0f779a822d
KotlinNLP Authors <github@kotlinnlp.com>
2018-02-01 10:27:14 +0100

    Update documentation, rename method

--- src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/birnn/BiRNNEncoder.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/birnn/BiRNNEncoder.kt
@@ -64,14 +64,22 @@ class BiRNNEncoder<InputNDArrayType: NDArray<InputNDArrayType>>(
   }
 
   /**
-   * Get the importance scores of the previous states (split by left and right) for each element of the last encoded
-   * sequence.
+   * Get the RAN importance scores of the previous states (split by left and right) for each element of the last
+   * encoded sequence.
+   * The scores values are in the range [0.0, 1.0].
+   *
+   * Each the left and the right scores are given following the linear order of the input sequence.
+   * E.g.: for an input sequence of 8 elements, the importance scores of the 4th element are related to the following
+   * elements:
+   * left -> [elm-1, elm-2, elm-3]
+   * right -> [elm-5, elm-6, elm-7, elm-8]
+   *
    * This method should be called only after an [encode] call.
    * It is required that the networks structures contain only a RAN layer.
    *
    * @return the list of importance scores pairs (left, right) of the input elements
    */
-  fun getImportanceScores(): List<Pair<DenseNDArray?, DenseNDArray?>> {
+  fun getRANImportanceScores(): List<Pair<DenseNDArray?, DenseNDArray?>> {
 
     val statesSize: Int = this.sequence.size
 
