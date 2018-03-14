047cb919b11fa860b92041f1ab320c85d21a0c0a
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-14 18:15:10 +0100

    Add competitive learning network

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/competitivelearning/CLNetwork.kt
@@ -0,0 +1,110 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.competitivelearning
+
+import com.kotlinnlp.simplednn.core.functionalities.losses.MSECalculator
+import com.kotlinnlp.simplednn.core.neuralprocessor.feedforward.FeedforwardNeuralProcessor
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.similarity
+
+/**
+ * The Competitive Learning Network.
+ *
+ * @property model the model of this network
+ */
+class CLNetwork(val model: CLNetworkModel) {
+
+  /**
+   * The scores of the last prediction.
+   */
+  val outputScores: Map<String, Double> get() = this.mutableScores
+
+  /**
+   * The mutable map which contains the scores of the last prediction.
+   */
+  private val mutableScores = mutableMapOf<String, Double>()
+
+  /**
+   * The map that associates each class to a feed-forward processor.
+   */
+  private val processors: Map<String, FeedforwardNeuralProcessor<DenseNDArray>> =
+    this.model.classes.associate { it to FeedforwardNeuralProcessor<DenseNDArray>(this.model.networks.getValue(it)) }
+
+  /**
+   * The loss calculator used to calculate the distance between the input and its reconstruction
+   */
+  private val lossCalculator = MSECalculator()
+
+  /**
+   * The processor used during the last learning.
+   */
+  private lateinit var lastUsedProcessor: FeedforwardNeuralProcessor<DenseNDArray>
+
+  /**
+   * Predict.
+   *
+   * @param inputArray the input array
+   *
+   * @return the highest scoring predicted class
+   */
+  fun predict(inputArray: DenseNDArray): String {
+
+    return this.processors.maxBy { (key, processor) ->
+
+      val score: Double = similarity(inputArray, processor.forward(inputArray))
+
+      this.mutableScores[key] = score
+
+      score
+    }!!
+      .key
+  }
+
+  /**
+   * Learn.
+   *
+   * @param inputArray the input array
+   * @param className the class to which the [inputArray] belongs
+   *
+   * @return
+   */
+  fun learn(inputArray: DenseNDArray, className: String): Double {
+
+    this.lastUsedProcessor = checkNotNull(this.processors[className]) {
+      "Unknown class: $className"
+    }
+
+    val reconstructedArray: DenseNDArray = this.lastUsedProcessor.forward(inputArray)
+
+    val errors: DenseNDArray = this.lossCalculator.calculateErrors(
+      output = reconstructedArray,
+      outputGold = inputArray)
+
+    this.lastUsedProcessor.backward(errors)
+
+    return this.lossCalculator.calculateLoss(
+      output = reconstructedArray,
+      outputGold = inputArray).avg()
+  }
+
+  /**
+   * @param copy a Boolean indicating whether the returned errors must be a copy or a reference
+   *
+   * @return the errors of the input
+   */
+  fun getInputErrors(copy: Boolean = true): DenseNDArray = this.lastUsedProcessor.getInputErrors(copy = copy)
+
+  /**
+   * @param copy a Boolean indicating whether the returned errors must be a copy or a reference
+   *
+   * @return the errors of the neural parameters
+   */
+  fun getParamsErrors(copy: Boolean) = CLNetworkParameters(
+    networksParams = this.processors.map { it.value.getParamsErrors(copy = copy) }
+  )
+}
---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/competitivelearning/CLNetworkModel.kt
@@ -0,0 +1,86 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.competitivelearning
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.functionalities.initializers.GlorotInitializer
+import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
+import com.kotlinnlp.simplednn.core.layers.LayerConfiguration
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.core.neuralnetwork.NeuralNetwork
+import com.kotlinnlp.simplednn.utils.Serializer
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.Serializable
+
+/**
+ * The model of a [CLNetwork].
+ */
+class CLNetworkModel(
+  val classes: Set<String>,
+  val inputType: LayerType.Input,
+  val inputSize: Int,
+  val hiddenSize: Int,
+  val hiddenActivation: ActivationFunction?,
+  weightsInitializer: Initializer? = GlorotInitializer(),
+  biasesInitializer: Initializer? = GlorotInitializer()
+) : Serializable {
+
+  /**
+   * The map that associates each class to a feed-forward network.
+   */
+  val networks: Map<String, NeuralNetwork> = this.classes.associate { it to NeuralNetwork(
+
+    LayerConfiguration(
+      size = this.inputSize,
+      inputType = this.inputType),
+
+    LayerConfiguration(
+      size = this.hiddenSize,
+      activationFunction = this.hiddenActivation,
+      connectionType = LayerType.Connection.Feedforward),
+
+    LayerConfiguration(
+      size = this.inputSize,
+      activationFunction = null,
+      connectionType = LayerType.Connection.Feedforward),
+
+    weightsInitializer = weightsInitializer,
+    biasesInitializer = biasesInitializer)
+  }
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed from Serializable)
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+
+    /**
+     * Read a [CLNetworkModel] (serialized) from an input stream and decode it.
+     *
+     * @param inputStream the [InputStream] from which to read the serialized [CLNetworkModel]
+     *
+     * @return the [CLNetworkModel] read from [inputStream] and decoded
+     */
+    fun load(inputStream: InputStream): CLNetworkModel = Serializer.deserialize(inputStream)
+  }
+
+  /**
+   * The parameters of all networks.
+   */
+  val params = CLNetworkParameters(this.networks.map { it.value.model })
+
+  /**
+   * Serialize this [CLNetworkModel] and write it to an output stream.
+   *
+   * @param outputStream the [OutputStream] in which to write this serialized [CLNetworkModel]
+   */
+  fun dump(outputStream: OutputStream) = Serializer.serialize(this, outputStream)
+}
