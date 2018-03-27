a2ab1ad75f3b4a63544cedfe6eee1930b3a47d66
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-27 22:58:46 +0200

    Add New Recirculation Network

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/newrecirculation/NewRecirculationModel.kt
@@ -0,0 +1,72 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.newrecirculation
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.functionalities.initializers.GlorotInitializer
+import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
+import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerParameters
+import com.kotlinnlp.simplednn.utils.Serializer
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.Serializable
+
+/**
+ * The model of the [NewRecirculationNetwork].
+ *
+ * @property inputSize the size of the input layer
+ * @property outputSize the size of the output layer
+ * @property activationFunction the activation function of the output (can be null)
+ * @property lambda the partition factor (default = 0.75)
+ * @param weightsInitializer the initializer of the weights (zeros if null, default: Glorot)
+ * @param biasesInitializer the initializer of the biases (zeros if null, default: Glorot)
+ */
+class NewRecirculationModel(
+  val inputSize: Int,
+  val outputSize: Int,
+  val activationFunction: ActivationFunction?,
+  val lambda: Double = 0.75,
+  weightsInitializer: Initializer? = GlorotInitializer(),
+  biasesInitializer: Initializer? = GlorotInitializer()
+) : Serializable {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+
+    /**
+     * Read a [NewRecirculationModel] (serialized) from an input stream and decode it.
+     *
+     * @param inputStream the [InputStream] from which to read the serialized [NewRecirculationModel]
+     *
+     * @return the [NewRecirculationModel] read from [inputStream] and decoded
+     */
+    fun load(inputStream: InputStream): NewRecirculationModel = Serializer.deserialize(inputStream)
+  }
+
+  /**
+   * The network parameters.
+   */
+  val params = FeedforwardLayerParameters(
+    inputSize = this.inputSize,
+    outputSize = this.outputSize,
+    weightsInitializer = weightsInitializer,
+    biasesInitializer = biasesInitializer
+  )
+
+  /**
+   * Serialize this [NewRecirculationModel] and write it to an output stream.
+   *
+   * @param outputStream the [OutputStream] in which to write this serialized [NewRecirculationModel]
+   */
+  fun dump(outputStream: OutputStream) = Serializer.serialize(this, outputStream)
+}
---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/newrecirculation/NewRecirculationNetwork.kt
@@ -0,0 +1,196 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.newrecirculation
+
+import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
+import com.kotlinnlp.simplednn.core.layers.feedforward.FeedforwardLayerParameters
+import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+
+/**
+ * New Recirculation network.
+ *
+ * @property model the network model
+ * @param recallThreshold the threshold of mean absolute error (between the real and the imaginary inputs) beyond
+ *                        which the recirculation process is triggered (default = 0.001)
+ * @param trainingLearningRate the learning rate of the training (default = 0.01)
+ */
+class NewRecirculationNetwork(
+  val model: NewRecirculationModel,
+  private val recallThreshold: Double = 0.001,
+  private val trainingLearningRate: Double = 0.01
+) {
+
+  companion object {
+
+    /**
+     * The max number of recall iterations.
+     */
+    private const val MAX_RECALL_ITERATIONS = 1.0e04
+  }
+
+  /**
+   *
+   */
+  private val realInput = AugmentedArray(values = DenseNDArrayFactory.zeros(Shape(this.model.inputSize)))
+
+  /**
+   *
+   */
+  private val realOutput = AugmentedArray(values = DenseNDArrayFactory.zeros(Shape(this.model.outputSize)))
+
+  /**
+   *
+   */
+  private val imaginaryInput = AugmentedArray(values = DenseNDArrayFactory.zeros(Shape(this.model.inputSize)))
+
+  /**
+   *
+   */
+  private val imaginaryOutput = AugmentedArray(values = DenseNDArrayFactory.zeros(Shape(this.model.outputSize)))
+
+  /**
+   *
+   */
+  private val meanAbsError: Double get() = this.realInput.values.sub(this.imaginaryInput.values).abs().sum()
+
+  /**
+   *
+   */
+  private val paramsErrors: FeedforwardLayerParameters = this.model.params.copy()
+
+  /**
+   * Set activation functions.
+   */
+  init {
+
+    if (this.model.activationFunction != null) {
+      this.realOutput.setActivation(this.model.activationFunction)
+      this.imaginaryOutput.setActivation(this.model.activationFunction)
+    }
+  }
+
+  /**
+   * Reconstruct a given array.
+   *
+   * @param inputArray the input array
+   * @param trainingMode whether the parameters must be trained during the re-entry
+   *
+   * @return the [inputArray] reconstruction
+   */
+  fun reconstruct(inputArray: DenseNDArray, trainingMode: Boolean = false): DenseNDArray {
+    require(inputArray.length == this.model.inputSize)
+
+    this.realInput.assignValues(inputArray)
+
+    this.calcImaginaryInput()
+
+    this.reEntry(trainingMode = trainingMode)
+
+    return this.imaginaryInput.values
+  }
+
+  /**
+   * Calculate the imaginary input.
+   * The [realInput] must be already set.
+   */
+  private fun calcImaginaryInput() {
+
+    val r: Double = this.model.lambda
+    val w: DenseNDArray = this.model.params.unit.weights.values as DenseNDArray
+    val b: DenseNDArray = this.model.params.unit.biases.values as DenseNDArray
+    val xR: DenseNDArray = this.realInput.values
+    val yR: DenseNDArray = this.realOutput.values
+    val xI: DenseNDArray = this.imaginaryInput.values
+
+    yR.assignDot(w, xR).assignSum(b)
+    this.realOutput.activate()
+
+    // Note of optimization: double transposition of two 1-dim arrays instead of a bigger 2-dim one
+    xI.assignSum(xR.prod(r), yR.t.dot(w).t.assignProd(1 - r))
+  }
+
+  /**
+   * Perform the re-entry technique for the recall process.
+   */
+  private fun reEntry(trainingMode: Boolean = false) {
+
+    var iterations = 0
+
+    while (this.meanAbsError >= this.recallThreshold || iterations++ >= MAX_RECALL_ITERATIONS) {
+
+      if (trainingMode) this.backward()
+
+      this.realInput.assignValues(this.imaginaryInput.values)
+
+      this.calcImaginaryInput()
+    }
+  }
+
+  /**
+   * Perform the backward calculating errors end updating the parameters.
+   */
+  private fun backward() {
+
+    val r: Double = this.model.lambda
+    val w: DenseNDArray = this.model.params.unit.weights.values as DenseNDArray
+    val b: DenseNDArray = this.model.params.unit.biases.values as DenseNDArray
+    val yR: DenseNDArray = this.realOutput.values
+    val xI: DenseNDArray = this.imaginaryInput.values
+    val yI: DenseNDArray = this.imaginaryOutput.values
+
+    yI.assignDot(w, xI).assignSum(b)
+    this.imaginaryOutput.activate()
+
+    yI.assignProd(1 - r).assignSum(yR.prod(r))
+
+    this.calcParamsErrors()
+    this.update()
+  }
+
+  /**
+   * Calculate the parameters errors, assign them and update the parameters.
+   *
+   *  gb = yI - yR
+   *  gw = (yI - yR) (dot) xI + ((xI - xR) (dot) yR)'
+   */
+  private fun calcParamsErrors() {
+
+    val xR: DenseNDArray = this.realInput.values
+    val yR: DenseNDArray = this.realOutput.values
+    val xI: DenseNDArray = this.imaginaryInput.values
+    val yI: DenseNDArray = this.imaginaryOutput.values
+
+    val gw: DenseNDArray = this.paramsErrors.unit.weights.values as DenseNDArray
+    val gb: DenseNDArray = this.paramsErrors.unit.biases.values as DenseNDArray
+
+    val gx: DenseNDArray = xI.sub(xR)
+    val gy: DenseNDArray = yI.sub(yR)
+
+    gw.assignDot(gy, xI.t).assignSum(yR.dot(gx.t))
+    gb.assignValues(gy)
+  }
+
+  /**
+   * Update the model parameters.
+   */
+  private fun update() {
+
+    val lr: Double = this.trainingLearningRate
+
+    val w: DenseNDArray = this.model.params.unit.weights.values as DenseNDArray
+    val b: DenseNDArray = this.model.params.unit.biases.values as DenseNDArray
+
+    val gw: DenseNDArray = this.paramsErrors.unit.weights.values as DenseNDArray
+    val gb: DenseNDArray = this.paramsErrors.unit.biases.values as DenseNDArray
+
+    w.assignSub(gw.assignProd(lr))
+    b.assignSub(gb.assignProd(lr))
+  }
+}
