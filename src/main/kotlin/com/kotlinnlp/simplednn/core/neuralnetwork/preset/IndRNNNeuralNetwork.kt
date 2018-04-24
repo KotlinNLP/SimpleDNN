8c661f93b7dfeb2612400ff0f6207c846d93bf55
KotlinNLP Authors <github@kotlinnlp.com>
2018-04-24 17:36:45 +0200

    Add IndRNN preset

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/neuralnetwork/preset/IndRNNNeuralNetwork.kt
@@ -0,0 +1,54 @@
+package com.kotlinnlp.simplednn.core.neuralnetwork.preset
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.ActivationFunction
+import com.kotlinnlp.simplednn.core.functionalities.initializers.GlorotInitializer
+import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.core.neuralnetwork.NeuralNetwork
+
+/**
+ * The IndRNN Neural Network factory.
+ */
+object IndRNNNeuralNetwork {
+
+  /**
+   * @param inputSize the size of the input layer
+   * @param inputType the type of the input layer (Dense -default-, Sparse, SparseBinary)
+   * @param inputDropout the dropout probability of the input (default 0.0). If applying it, the usual value is 0.25.
+   * @param hiddenSize the size of the hidden layers
+   * @param hiddenActivation the activation function of the hidden layers
+   * @param hiddenDropout the dropout probability of the hidden layers (default 0.0)
+   * @param numOfHidden the number of hidden layers (must be >= 0, default 1)
+   * @param outputSize the size of the output layer
+   * @param outputActivation the activation function of the output layer
+   * @param weightsInitializer the initializer of the weights (zeros if null, default: Glorot)
+   * @param biasesInitializer the initializer of the biases (zeros if null, default: Glorot)
+   */
+  operator fun invoke(inputSize: Int,
+                      inputType: LayerType.Input = LayerType.Input.Dense,
+                      inputDropout: Double = 0.0,
+                      hiddenSize: Int,
+                      hiddenActivation: ActivationFunction?,
+                      hiddenDropout: Double = 0.0,
+                      numOfHidden: Int = 1,
+                      outputSize: Int,
+                      outputActivation: ActivationFunction?,
+                      weightsInitializer: Initializer? = GlorotInitializer(),
+                      biasesInitializer: Initializer? = GlorotInitializer()): NeuralNetwork =
+    GenericNeuralNetwork(
+      inputSize = inputSize,
+      inputType = inputType,
+      inputDropout = inputDropout,
+      hiddenSize = hiddenSize,
+      hiddenActivation = hiddenActivation,
+      hiddenDropout = hiddenDropout,
+      hiddenMeProp = false,
+      hiddenConnection = LayerType.Connection.IndRNN,
+      numOfHidden = numOfHidden,
+      outputSize = outputSize,
+      outputActivation = outputActivation,
+      outputMeProp = false,
+      weightsInitializer = weightsInitializer,
+      biasesInitializer = biasesInitializer
+    )
+}
