ba5fd6fce9904c892ff39cd7088c54e9150114f3
KotlinNLP Authors <github@kotlinnlp.com>
2018-06-05 12:49:34 +0200

    Update documentation

--- src/main/kotlin/com/kotlinnlp/simplednn/core/mergelayers/affine/AffineLayerParameters.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/mergelayers/affine/AffineLayerParameters.kt
@@ -39,7 +39,7 @@ open class AffineLayerParameters(
   companion object {
 
     /**
-     * Private val used to serialize the class (needed from Serializable)
+     * Private val used to serialize the class (needed by Serializable).
      */
     @Suppress("unused")
     private const val serialVersionUID: Long = 1L
--- src/main/kotlin/com/kotlinnlp/simplednn/core/mergelayers/sum/SumLayerParameters.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/mergelayers/sum/SumLayerParameters.kt
@@ -28,7 +28,7 @@ class SumLayerParameters(
   companion object {
 
     /**
-     * Private val used to serialize the class (needed from Serializable)
+     * Private val used to serialize the class (needed by Serializable).
      */
     @Suppress("unused")
     private const val serialVersionUID: Long = 1L
