e5ebbb43d4078887682baeed86746feb7c29fadc
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-12 01:49:30 +0200

    Change the NeuralProcessor from abstract class to interface and add the basic methods and properties common to virtually all the implemented processors/encoders

--- src/main/kotlin/com/kotlinnlp/simplednn/core/neuralprocessor/NeuralProcessor.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/neuralprocessor/NeuralProcessor.kt
@@ -7,22 +7,86 @@
 
 package com.kotlinnlp.simplednn.core.neuralprocessor
 
-import com.kotlinnlp.simplednn.core.neuralnetwork.NetworkParameters
-import com.kotlinnlp.simplednn.core.neuralnetwork.NeuralNetwork
+import com.kotlinnlp.simplednn.core.optimizer.Optimizer
 import com.kotlinnlp.utils.ItemsPool
 
 /**
- * The [NeuralProcessor] acts on the [neuralNetwork] performing predictions and training.
- *
- * @property neuralNetwork a [NeuralNetwork]
- * @property id an identification number useful to track a specific processor
+ * The NeuralProcessor interface.
  */
-abstract class NeuralProcessor(val neuralNetwork: NeuralNetwork, override val id: Int = 0) : ItemsPool.IDItem {
+interface NeuralProcessor<
+  in InputType : Any,
+  out OutputType : Any,
+  in ErrorsType : Any,
+  out InputErrorsType : Any,
+  out ParamsErrorsType : Any
+  > : ItemsPool.IDItem {
 
   /**
-   * @param copy a Boolean indicating whether the returned errors must be a copy or a reference
-   *
-   * @return the errors of the parameters
+   * The InputErrorsType to use in case the neural processor does not provide input errors.
    */
-  abstract fun getParamsErrors(copy: Boolean = true): NetworkParameters
+  object NoInputErrors
+
+  /**
+   * Whether to apply the dropout during the [forward] (if supported)
+   */
+  val useDropout: Boolean
+
+  /**
+   * Whether to propagate the errors to the input during the [backward] (if supported)
+   */
+  val propagateToInput: Boolean
+
+  /**
+   * The Forward.
+   *
+   * @param input the input
+   *
+   * @return the result of the forward
+   */
+  fun forward(input: InputType): OutputType
+
+  /**
+   * The Backward.
+   *
+   * @param outputErrors the output errors
+   */
+  fun backward(outputErrors: ErrorsType)
+
+  /**
+   * Return the input errors of the last backward.
+   * Before calling this method make sure that [propagateToInput] is enabled.
+   *
+   * @param copy whether to return by value or by reference (default true)
+   *
+   * @return the input errors
+   */
+  fun getInputErrors(copy: Boolean = true): InputErrorsType
+
+  /**
+   * Return the params errors of the last backward.
+   *
+   * @param copy a Boolean indicating whether the returned errors must be a copy or a reference (default true)
+   *
+   * @return the parameters errors
+   */
+  fun getParamsErrors(copy: Boolean = true): ParamsErrorsType
+
+  /**
+   * Back-propagate the [errors] through the network, accumulate the resulting params errors in the
+   * [optimizer] and returns the input errors.
+   *
+   * Before calling this method make sure that [propagateToInput] is enabled.
+   *
+   * @param errors the output errors
+   * @param optimizer the optimizer
+   * @param copy a Boolean indicating whether the errors must be a copy or a reference (default false)
+   *
+   * @return the input errors
+   */
+  fun propagateErrors(errors: ErrorsType, optimizer: Optimizer<ParamsErrorsType>, copy: Boolean = false): InputErrorsType {
+
+    backward(errors)
+    optimizer.accumulate(getParamsErrors(copy = copy))
+    return getInputErrors(copy = copy)
+  }
 }
