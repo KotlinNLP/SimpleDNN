0d97af965072ca595c77efbef97a93d540f1c304
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-04 16:51:17 +0100

    Make the Softmax an extension of the SoftmaxBase

--- src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/activations/Softmax.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/activations/Softmax.kt
@@ -7,62 +7,19 @@
 
 package com.kotlinnlp.simplednn.core.functionalities.activations
 
-import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
 
 /**
- * The softmax function transforms a vector in non-negative real numbers that
- * lie between zero and one and sum to one.
- * Its output is interpretable as a discrete probability distribution over k possible outcomes.
+ * The softmax function transforms the values of a vector in real numbers in the range [0.0, 1.0].
  *
+ * (Note: as their sum is always 1.0 they can be interpreted as a discrete probability distribution).
+ *
+ * Attention!
+ * This class is an optimized extension of the base Softmax.
+ * It should be used as output activation of neural modules that use the difference with the gold value as errors.
  */
-class Softmax : ActivationFunction {
-
-  /**
-   * Assign to [out] the result of the Softmax function calculated respect to the input array.
-   *
-   * @param array the input array
-   * @param out the NDArray in which the result is written
-   */
-  override fun f(array: DenseNDArray, out: DenseNDArray) {
-
-    val max: Double = array[array.argMaxIndex()]
-    var sum = 0.0
-
-    for (i in 0 until array.length) {
-      val e: Double = Math.exp(array[i] - max)
-      out[i] = e
-      sum += e
-    }
-
-    out.assignDiv(sum)
-  }
-
-  /**
-   * Calculate the Softmax derivative respect to the input array.
-   *
-   * @param xArray the input array
-   *
-   * @return a new NDArray containing the result (the Jacobian matrix)
-   */
-  override fun df(xArray: DenseNDArray): DenseNDArray {
-
-    val jacobianMatrix: DenseNDArray = DenseNDArrayFactory.zeros(shape = Shape(xArray.length, xArray.length))
-
-    for (i in 0 until xArray.length) {
-      for (j in i until xArray.length) {
-
-        jacobianMatrix[i, j] = if (i == j) xArray[i] * (1.0 - xArray[j]) else - xArray[i] * xArray[j]
-
-        if (i != j) {
-          jacobianMatrix[j, i] = jacobianMatrix[i, j]
-        }
-      }
-    }
-
-    return jacobianMatrix
-  }
+class Softmax : SoftmaxBase() {
 
   /**
    * Calculate the Softmax derivative respect to the input array already activated, as optimization.
