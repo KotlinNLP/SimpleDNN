0877108ec9fc5f2e534dadb44b027b26ac736019
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-25 11:02:24 +0100

    Add secondary constructors

--- src/main/kotlin/com/kotlinnlp/simplednn/core/embeddings/Embedding.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/embeddings/Embedding.kt
@@ -8,6 +8,10 @@
 package com.kotlinnlp.simplednn.core.embeddings
 
 import com.kotlinnlp.simplednn.core.arrays.UpdatableDenseArray
+import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
+import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
 import java.io.Serializable
 
 /**
@@ -26,6 +30,46 @@ data class Embedding(val id: Int, val array: UpdatableDenseArray) : Serializable
     @Suppress("unused")
     private const val serialVersionUID: Long = 1L
   }
+
+  /**
+   * Build a new [Embedding] with the given [id] and [vector].
+   *
+   * @param id the id of the embedding
+   * @param vector the vector of the embedding
+   *
+   * @return a new embedding
+   */
+  constructor(id: Int, vector: DoubleArray): this(
+    id = id,
+    array = UpdatableDenseArray(values = DenseNDArrayFactory.arrayOf(vector))
+  )
+
+  /**
+   * Build a new [Embedding] with the given [id] and [vector].
+   *
+   * @param id the id of the embedding
+   * @param vector the vector of the embedding
+   *
+   * @return a new embedding
+   */
+  constructor(id: Int, vector: DenseNDArray): this(
+    id = id,
+    array = UpdatableDenseArray(values = vector)
+  )
+
+  /**
+   * Build a new [Embedding] with the given [id] and [size].
+   *
+   * @param id the id of the embedding
+   * @param size the size of the embedding
+   * @param initializer the initializer of the values (can be null)
+   *
+   * @return a new [Embedding] with the given [id]
+   */
+  constructor(id: Int, size: Int, initializer: Initializer?): this(
+    id = id,
+    array = UpdatableDenseArray(Shape(size)).apply { initializer?.initialize(values) }
+  )
 
   /**
    * @param digits precision specifier
