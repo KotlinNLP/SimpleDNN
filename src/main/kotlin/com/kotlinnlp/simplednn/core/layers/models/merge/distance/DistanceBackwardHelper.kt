832985b92b0f883a9cc493ce00e485454d9db7b9
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-06 20:03:29 +0100

    Add Backward helper

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/layers/models/merge/distance/DistanceBackwardHelper.kt
@@ -0,0 +1,40 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.layers.models.merge.distance
+
+import com.kotlinnlp.simplednn.core.layers.LayerParameters
+import com.kotlinnlp.simplednn.core.layers.helpers.BackwardHelper
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+
+class DistanceBackwardHelper <InputNDArrayType : NDArray<InputNDArrayType>>(
+    override val layer: DistanceLayer<InputNDArrayType>
+) : BackwardHelper<InputNDArrayType> {
+
+  /**
+   * Executes the backward calculating the errors of the parameters and eventually of the input through the SGD
+   * algorithm, starting from the preset errors of the output array.
+   *
+   * @param paramsErrors the errors of the parameters which will be filled
+   * @param propagateToInput whether to propagate the errors to the input array
+   */
+  override fun backward(paramsErrors: LayerParameters<*>, propagateToInput: Boolean) {
+
+    if (propagateToInput) {
+      this.assignLayerGradients()
+    }
+  }
+
+  /**
+   * Assign the the layer gradients.
+   */
+  private fun assignLayerGradients() {
+
+    TODO("Not implemented")
+  }
+}
