1ebfd82f8606b3f34a32e6beae301dcd7673c36c
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-06 20:05:48 +0100

    Add distance layer forward test

---
+++ src/test/kotlin/core/layers/merge/distance/DistanceLayerUtils.kt
@@ -0,0 +1,29 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package core.layers.merge.distance
+
+import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
+import com.kotlinnlp.simplednn.core.layers.models.merge.distance.DistanceLayer
+import com.kotlinnlp.simplednn.core.layers.models.merge.distance.DistanceLayerParameters
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+
+object DistanceLayerUtils {
+  /**
+   *
+   */
+  fun buildLayer(): DistanceLayer<DenseNDArray> = DistanceLayer(
+      inputArray1 = AugmentedArray(values = DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.7, -0.7, 0.8, 0.6))),
+      inputArray2 = AugmentedArray(values = DenseNDArrayFactory.arrayOf(doubleArrayOf(0.5, -0.4, 0.8, -0.7))),
+      params = DistanceLayerParameters(inputSize = 4))
+
+  /**
+   *
+   */
+  fun getOutputErrors() = DenseNDArrayFactory.arrayOf(doubleArrayOf(-0.8))
+}
