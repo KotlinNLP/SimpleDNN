e89855f0329164e84aa717c1771053eeb496c5e6
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-14 19:54:09 +0100

    Add Alias

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/optimizer/Alias.kt
@@ -0,0 +1,12 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.optimizer
+
+import com.kotlinnlp.simplednn.core.arrays.ParamsArray
+
+typealias ParamsErrorsList = List<ParamsArray<*>.ParamsErrors<*>>
