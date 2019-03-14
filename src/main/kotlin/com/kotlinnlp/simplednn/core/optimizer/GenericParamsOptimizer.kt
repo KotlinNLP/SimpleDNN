4dd27c9b020067df7d182d7cb30ff07e69a7f9ca
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-15 00:44:24 +0100

    Add GenericParamsOptimizer

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/optimizer/GenericParamsOptimizer.kt
@@ -0,0 +1,105 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.optimizer
+
+import com.kotlinnlp.simplednn.core.arrays.UpdatableDenseArray
+import com.kotlinnlp.simplednn.core.functionalities.updatemethods.UpdateMethod
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.sparse.SparseNDArray
+import com.kotlinnlp.simplednn.utils.scheduling.BatchScheduling
+import com.kotlinnlp.simplednn.utils.scheduling.EpochScheduling
+import com.kotlinnlp.simplednn.utils.scheduling.ExampleScheduling
+
+/**
+ * The optimizer of neural parameters.
+ *
+ * @param updateMethod the update method helper (Learning Rate, ADAM, AdaGrad, ...)
+ */
+class GenericParamsOptimizer(private val updateMethod: UpdateMethod<*>) : ScheduledUpdater {
+
+  /**
+   * The accumulator of parameters errors.
+   */
+  private val paramsErrorsAccumulator = GenericParamsErrorsAccumulator()
+
+  /**
+   * Calculate the errors average, update the parameters.
+   */
+  override fun update() {
+
+    if (this.paramsErrorsAccumulator.isNotEmpty) {
+      
+      this.paramsErrorsAccumulator.averageErrors()
+      this.updateParams()
+      this.paramsErrorsAccumulator.clear()
+    }
+  }
+
+  /**
+   * Accumulate the given [paramsErrors] into the accumulator.
+   *
+   * @param paramsErrors the parameters errors to accumulate
+   * @param copy a Boolean indicating if the [paramsErrors] can be used as reference or must be copied. Set copy = false
+   *             to optimize the accumulation when the amount of the errors to accumulate is 1. (default = true)
+   */
+  fun accumulate(paramsErrors: ParamsErrorsList, copy: Boolean = true) {
+
+    this.paramsErrorsAccumulator.accumulate(paramsErrors = paramsErrors, copy = copy)
+  }
+
+  /**
+   * Method to call every new epoch.
+   * In turn it calls the same method into the `updateMethod`
+   */
+  override fun newEpoch() {
+
+    if (this.updateMethod is EpochScheduling) {
+      this.updateMethod.newEpoch()
+    }
+  }
+
+  /**
+   * Method to call every new batch.
+   * In turn it calls the same method into the `updateMethod`
+   */
+  override fun newBatch() {
+
+    if (this.updateMethod is BatchScheduling) {
+      this.updateMethod.newBatch()
+    }
+  }
+
+  /**
+   * Method to call every new example.
+   * In turn it calls the same method into the `updateMethod`
+   */
+  override fun newExample() {
+
+    if (this.updateMethod is ExampleScheduling) {
+      this.updateMethod.newExample()
+    }
+  }
+
+  /**
+   * Update the parameters in respect of the errors using the update method helper (Learning Rate, ADAM, AdaGrad, ...).
+   */
+  private fun updateParams() {
+
+    this.paramsErrorsAccumulator.getParamsErrors(copy = false).forEach {
+
+      val params = it.refParams
+      val errors = it.values
+
+      when (errors) {
+        is DenseNDArray -> this.updateMethod.update(array = params as UpdatableDenseArray, errors = errors)
+        is SparseNDArray -> this.updateMethod.update(array = params as UpdatableDenseArray, errors = errors)
+        else -> throw RuntimeException("Invalid errors type")
+      }
+    }
+  }
+}
---
+++ src/test/kotlin/core/optimizer/GenericParamsOptimizerSpec.kt
@@ -0,0 +1,68 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package core.optimizer
+
+import com.kotlinnlp.simplednn.core.functionalities.updatemethods.learningrate.LearningRateMethod
+import com.kotlinnlp.simplednn.core.optimizer.GenericParamsOptimizer
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertTrue
+
+/**
+ *
+ */
+class GenericParamsOptimizerSpec : Spek({
+
+  describe("a GenericParamsOptimizer") {
+
+    val learningRateMethod = LearningRateMethod(learningRate = 0.1)
+
+    context("update after accumulate") {
+
+      val optimizer = GenericParamsOptimizer(learningRateMethod)
+
+      val params = GenericParamsOptimizerUtils.buildParams()
+
+      val gw1 = params[0].buildDenseErrors(GenericParamsOptimizerUtils.buildWeightsErrorsValues1())
+      val gb1 = params[1].buildDenseErrors(GenericParamsOptimizerUtils.buildBiasesErrorsValues1())
+      val gw2 = params[0].buildDenseErrors(GenericParamsOptimizerUtils.buildWeightsErrorsValues2())
+      val gb2 = params[1].buildDenseErrors(GenericParamsOptimizerUtils.buildBiasesErrorsValues2())
+
+      optimizer.accumulate(listOf(gw1, gb1, gw2, gb2))
+      optimizer.update()
+
+      val w: DenseNDArray = params[0].values
+      val b: DenseNDArray = params[1].values
+
+      it("should match the expected updated weights") {
+        assertTrue {
+          w.equals(
+            DenseNDArrayFactory.arrayOf(listOf(
+              doubleArrayOf(0.25, 0.42, 0.185, -0.16),
+              doubleArrayOf(0.15, -0.125, 0.14, 0.58)
+            )),
+            tolerance = 1.0e-06
+          )
+        }
+      }
+
+      it("should match the expected updated biases") {
+        assertTrue {
+          b.equals(
+            DenseNDArrayFactory.arrayOf(doubleArrayOf(0.33, -0.385)),
+            tolerance = 1.0e-06
+          )
+        }
+      }
+    }
+  }
+})
