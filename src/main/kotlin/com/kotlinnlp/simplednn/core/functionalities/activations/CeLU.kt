bef8293577a75ac7263646837d707d12ad2370ee
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-30 22:48:51 +0100

    Add CeLU activation function

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/activations/CeLU.kt
@@ -0,0 +1,56 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.functionalities.activations
+
+/**
+ * CeLU(x) = max(0, x) + min(0, α ∗ (exp(x / α) − 1)))
+ *
+ * References
+ * [Jonathan T. Barron, 2017, Continuously Differentiable Exponential Linear Units](https://arxiv.org/abs/1704.07483)
+ *
+ * @property alpha defines the decreasing exponential rate for the negative values. Defaults to 1.0
+ * Using default value, the function is identical to ELU.
+ *
+ */
+class CeLU (val alpha: Double = 1.0) : ScalarActivationFunction() {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+
+  /**
+   * Check if alpha is positive
+   */
+  init {
+    require(this.alpha > 0.0)
+  }
+
+  /**
+   * Calculate the CeLU function in [x].
+   *
+   * @param x input
+   *
+   * @return f([x])
+   */
+  override fun f(x: Double): Double = if (x > 0.0) x else this.alpha * (Math.exp(x / this.alpha) - 1.0)
+
+  /**
+   * Optimized derivative of the CeLU function, calculated respect to the input already activated.
+   *
+   * @param fx the input already activated [f(x)]
+   *
+   * @return the CeLU derivative calculated in x
+   */
+  override fun dfOptimized(fx: Double): Double = if (fx > 0.0) 1.0 else (fx + this.alpha) / this.alpha
+
+}
---
+++ src/test/kotlin/core/functionalities/activations/CeLUSpec.kt
@@ -0,0 +1,58 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package core.functionalities.activations
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.CeLU
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertTrue
+
+/**
+ *
+ */
+
+class CeLUSpec: Spek({
+
+  describe("a CeLU activation function") {
+
+    val array = DenseNDArrayFactory.arrayOf(doubleArrayOf(0.0, 0.1, 0.01, -0.1, -0.01, 1.0, 10.0, -1.0, -10.0))
+
+    context("default configuration") {
+
+      val activationFunction = CeLU(alpha = 2.0)
+      val activatedArray = activationFunction.f(array)
+
+      on("f") {
+
+        val expectedArray = DenseNDArrayFactory.arrayOf(doubleArrayOf(
+            0.0, 0.1, 0.01, -0.097541151, -0.009975042, 1.0, 10.0, -0.786938681, -1.986524106
+        ))
+
+        it("should return the expected errors") {
+          assertTrue { expectedArray.equals(activatedArray, tolerance = 1.0e-08) }
+        }
+      }
+
+      on("dfOptimized") {
+
+        val dfArray = activationFunction.dfOptimized(activatedArray)
+        val expectedArray = DenseNDArrayFactory.arrayOf(doubleArrayOf(
+            1.0, 1.0, 1.0, 0.95122942, 0.995012479, 1.0, 1.0, 0.606530659, 0.006737947
+        ))
+
+        it("should return the expected errors") {
+          assertTrue { expectedArray.equals(dfArray, tolerance = 1.0e-08) }
+        }
+      }
+    }
+  }
+})
