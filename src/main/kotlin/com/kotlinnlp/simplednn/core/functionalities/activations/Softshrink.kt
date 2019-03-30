c603ffe9a1f49cfbb5957182e430af70b208ff14
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-30 22:53:12 +0100

    Add Softshrink activation function

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/activations/Softshrink.kt
@@ -0,0 +1,54 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, you can obtain one at http://mozilla.org/MPL/2.0/.
+* ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.core.functionalities.activations
+
+/**
+ * Softshrink(x) =
+ * x - lambda, if x > lambda
+ * x + lambda, if x < lambda
+ * 0, otherwise
+ *
+ * @property lambda defines the lambda value
+ */
+class Softshrink(val lambda: Double = 0.5) : ScalarActivationFunction() {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+
+  /**
+   * Check if lambda is positive
+   */
+  init {
+    require(this.lambda > 0.0)
+  }
+
+  /**
+   * Calculate the Softplus function in [x].
+   *
+   * @param x input
+   *
+   * @return f([x])
+   */
+  override fun f(x: Double): Double = if (x > this.lambda) x - this.lambda else if (x < -this.lambda) x + this.lambda else 0.0
+
+  /**
+   * Optimized derivative of the Softplus function, calculated respect to the input already activated.
+   *
+   * @param fx the input already activated [f(x)]
+   *
+   * @return the Softplus derivative calculated in x
+   */
+  override fun dfOptimized(fx: Double): Double = if (fx + this.lambda > this.lambda) 1.0 else if (fx - this.lambda < -this.lambda) 1.0 else 0.0
+
+}
