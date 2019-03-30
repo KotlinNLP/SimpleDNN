e079836729a9321ff14509388a04212c89566edf
KotlinNLP Authors <github@kotlinnlp.com>
2019-03-30 22:50:00 +0100

    Add hard Sigmoid activation function

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/functionalities/activations/HardSigmoid.kt
@@ -0,0 +1,37 @@
+package com.kotlinnlp.simplednn.core.functionalities.activations
+
+/**
+ * The Hardsigmoid(x) activation function,
+ * transforming the values x into the range [0, 1].
+ */
+
+class HardSigmoid: ScalarActivationFunction() {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+
+  /**
+   * Calculate the activation function in [x].
+   *
+   * @param x input
+   *
+   * @return f([x])
+   */
+  override fun f(x: Double): Double = if (x > 2.5) 1.0 else if (x < -2.5) 0.0 else 0.2 * x + 0.5
+
+  /**
+   * Optimized derivative of the HardSigmoid function, calculated respect to the input already activated.
+   *
+   * @param fx the input already activated [f(x)]
+   *
+   * @return the HardSigmoid derivative calculated in x
+   */
+  override fun dfOptimized(fx: Double): Double = if ((fx - 0.5) / 0.2 < 2.5 && (fx - 0.5) / 0.2 > -2.5) 0.2 else 0.0
+
+}
---
+++ src/test/kotlin/core/functionalities/activations/HardSigmoidSpec.kt
@@ -0,0 +1,53 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package core.functionalities.activations
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.HardSigmoid
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertTrue
+
+/**
+ *
+ */
+
+class HardSigmoidSpec : Spek({
+
+  describe("an HardSigmoid activation function") {
+
+    val activationFunction = HardSigmoid()
+    val array = DenseNDArrayFactory.arrayOf(doubleArrayOf(0.0, 0.1, 0.01, -0.1, -0.01, 1.0, 10.0, -1.0, -10.0))
+    val activatedArray = activationFunction.f(array)
+
+    on("f") {
+
+      val expectedArray = DenseNDArrayFactory.arrayOf(doubleArrayOf(
+          0.5, 0.52, 0.502, 0.48, 0.498, 0.7, 1.0, 0.3, 0.0
+      ))
+
+      it("should return the expected errors") {
+        assertTrue { expectedArray.equals(activatedArray, tolerance = 1.0e-08) }
+      }
+    }
+
+    on("dfOptimized") {
+
+      val dfArray = activationFunction.dfOptimized(activatedArray)
+      val expectedArray = DenseNDArrayFactory.arrayOf(doubleArrayOf(
+          0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.0, 0.2, 0.0
+      ))
+
+      it("should return the expected errors") {
+        assertTrue { expectedArray.equals(dfArray, tolerance = 1.0e-08) }
+      }
+    }
+  }
+})
