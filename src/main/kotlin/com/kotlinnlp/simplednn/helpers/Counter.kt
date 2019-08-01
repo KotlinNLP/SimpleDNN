5721cc41335507bd7ff31dd92e0713a15f728e52
KotlinNLP Authors <github@kotlinnlp.com>
2019-08-01 22:32:38 +0200

    Move TrainingStatistics from the 'examples' package into the 'helpers'

--- src/main/kotlin/com/kotlinnlp/simplednn/helpers/Counter.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/helpers/Counter.kt
@@ -1,58 +1,54 @@
-/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+/* Copyright 2019-present The KotlinNLP Authors. All Rights Reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package traininghelpers.training
+package com.kotlinnlp.simplednn.helpers
 
 /**
- *
+ * A counter of values used during the training.
  */
-data class TrainingStatistics(
-  var epochCount: Int = 0,
-  var batchCount: Int = 0,
+internal data class Counter(
+  var epoch: Int = 0,
+  var batch: Int = 0,
   var exampleCount: Int = 0,
-  var lastLoss: Double = 0.0,
-  var lastAccuracy: Double = 0.0) {
+  var bestAccuracy: Double = 0.0
+) {
 
   /**
-   *
+   * Reset all the counters.
    */
   fun reset() {
-    this.epochCount = 0
-    this.batchCount = 0
+    this.epoch = 0
+    this.batch = 0
     this.exampleCount = 0
-    this.lastLoss  = 0.0
-    this.lastAccuracy = 0.0
+    this.bestAccuracy = 0.0
   }
 
   /**
    * Method to call every new epoch.
-   *
-   * It increments the epochCount and sets the batchCount and the exampleCount to zero
+   * It increments the epochs counter and it sets the batches and the examples counters to zero.
    */
   fun newEpoch() {
-    this.epochCount += 1
-    this.batchCount = 0
+    this.epoch += 1
+    this.batch = 0
     this.exampleCount = 0
   }
 
   /**
    * Method to call every new batch.
-   *
-   * It increments the batchCount and sets the exampleCount to zero
+   * It increments the batches counter and it sets the examples counter to zero.
    */
   fun newBatch() {
-    this.batchCount += 1
+    this.batch += 1
     this.exampleCount = 0
   }
 
   /**
    * Method to call every new example.
-   *
-   * It increments the exampleCount
+   * It increments the examples counter.
    */
   fun newExample() {
     this.exampleCount += 1
