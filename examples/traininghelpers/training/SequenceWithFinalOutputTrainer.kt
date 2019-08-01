dce2dd32877fe055e7ee339361d67f11e0e61642
KotlinNLP Authors <github@kotlinnlp.com>
2019-08-01 22:34:27 +0200

    Update examples helpers extending the SimpleDNN helpers

---
+++ examples/traininghelpers/training/SequenceWithFinalOutputTrainer.kt
@@ -0,0 +1,109 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package traininghelpers.training
+
+import com.kotlinnlp.simplednn.core.functionalities.activations.Softmax
+import com.kotlinnlp.simplednn.core.functionalities.losses.SoftmaxCrossEntropyCalculator
+import com.kotlinnlp.simplednn.core.functionalities.losses.LossCalculator
+import com.kotlinnlp.simplednn.core.functionalities.updatemethods.UpdateMethod
+import com.kotlinnlp.simplednn.core.layers.StackedLayersParameters
+import com.kotlinnlp.simplednn.core.neuralprocessor.recurrent.RecurrentNeuralProcessor
+import com.kotlinnlp.simplednn.core.optimizer.ParamsOptimizer
+import com.kotlinnlp.simplednn.helpers.Evaluator
+import com.kotlinnlp.simplednn.helpers.Trainer
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.utils.Shuffler
+import utils.SequenceExampleWithFinalOutput
+
+/**
+ * A helper to train a recurrent model with sequence examples with final output.
+ *
+ * @param model the neural model to train
+ * @param updateMethod the update method helper (Learning Rate, ADAM, AdaGrad, ...)
+ * @param lossCalculator a loss calculator
+ * @param examples the training examples
+ * @param epochs the number of training epochs
+ * @param batchSize the size of each batch (default 1)
+ * @param evaluator the helper for the evaluation (default null)
+ * @param shuffler used to shuffle the examples before each epoch (with pseudo random by default)
+ * @param verbose whether to print info about the training progress and timing (default = true)
+ */
+class SequenceWithFinalOutputTrainer<NDArrayType: NDArray<NDArrayType>>(
+  model: StackedLayersParameters,
+  updateMethod: UpdateMethod<*>,
+  private val lossCalculator: LossCalculator,
+  examples: List<SequenceExampleWithFinalOutput<NDArrayType>>,
+  epochs: Int,
+  batchSize: Int = 1,
+  evaluator: Evaluator<SequenceExampleWithFinalOutput<NDArrayType>>? = null,
+  shuffler: Shuffler = Shuffler(),
+  verbose: Boolean = true
+) : Trainer<SequenceExampleWithFinalOutput<NDArrayType>>(
+  params = model,
+  modelFilename = "",
+  optimizers = listOf(ParamsOptimizer(updateMethod)),
+  examples = examples,
+  epochs = epochs,
+  batchSize = batchSize,
+  evaluator = evaluator,
+  shuffler = shuffler,
+  verbose = verbose
+) {
+
+  /**
+   * The neural processor that uses the model.
+   */
+  private val neuralProcessor: RecurrentNeuralProcessor<NDArrayType> = RecurrentNeuralProcessor(
+    model = model,
+    useDropout = false,
+    propagateToInput = false)
+
+  /**
+   * Require softmax cross-entropy loss to be used with the softmax as output activation function and vice versa.
+   */
+  init {
+
+    val activation = this.neuralProcessor.model.layersConfiguration.last().activationFunction
+
+    require(
+      (this.lossCalculator is SoftmaxCrossEntropyCalculator && activation is Softmax) ||
+        (this.lossCalculator !is SoftmaxCrossEntropyCalculator && activation !is Softmax)
+    ) {
+      "Softmax cross-entropy loss must be used with the softmax as output activation function and vice versa"
+    }
+  }
+
+  /**
+   * Learn from an example (forward + backward).
+   *
+   * @param example the example used to train the network
+   */
+  override fun learnFromExample(example: SequenceExampleWithFinalOutput<NDArrayType>) {
+
+    this.neuralProcessor.forward(example.sequenceFeatures)
+
+    val output: DenseNDArray = this.neuralProcessor.getOutput(copy = true)
+    val errors: DenseNDArray = this.lossCalculator.calculateErrors(output = output, outputGold = example.outputGold)
+
+    this.neuralProcessor.backward(errors)
+  }
+
+  /**
+   * Accumulate the errors of the model resulting after the call of [learnFromExample].
+   */
+  override fun accumulateErrors() {
+    this.optimizers.single().accumulate(
+      this.neuralProcessor.getParamsErrors(copy = this.batchSize > 1), copy = this.batchSize > 1)
+  }
+
+  /**
+   * Dump the [params] to file.
+   */
+  override fun dumpModel() {}
+}
---
+++ examples/traininghelpers/validation/SequenceEvaluator.kt
@@ -0,0 +1,76 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package traininghelpers.validation
+
+import com.kotlinnlp.simplednn.core.neuralprocessor.recurrent.RecurrentNeuralProcessor
+import utils.SequenceExample
+import com.kotlinnlp.simplednn.core.functionalities.outputevaluation.OutputEvaluationFunction
+import com.kotlinnlp.simplednn.core.layers.StackedLayersParameters
+import com.kotlinnlp.simplednn.helpers.Statistics
+import com.kotlinnlp.simplednn.helpers.Evaluator
+import com.kotlinnlp.simplednn.simplemath.ndarray.NDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+
+/**
+ * A helper which evaluates a dataset of [SequenceExample]s.
+ *
+ * @param model the model to validate
+ * @param examples a list of examples to validate
+ * @param outputEvaluationFunction the output evaluation function
+ * @param saveContributions whether to save the contributions of each input to its output (needed to calculate the
+ *                          relevance, default false)
+ * @param afterEachEvaluation a callback called after each example evaluation
+ * @param verbose whether to print info about the validation progress (default = true)
+ */
+class SequenceEvaluator<NDArrayType: NDArray<NDArrayType>>(
+  model: StackedLayersParameters,
+  examples: List<SequenceExample<NDArrayType>>,
+  private val outputEvaluationFunction: OutputEvaluationFunction,
+  private val afterEachEvaluation: (example: SequenceExample<NDArrayType>,
+                                    isCorrect: Boolean,
+                                    processor: RecurrentNeuralProcessor<NDArrayType>) -> Unit = { _, _, _ -> },
+  private val saveContributions: Boolean = false,
+  verbose: Boolean = true
+) : Evaluator<SequenceExample<NDArrayType>>(examples = examples, params = model, verbose = verbose) {
+
+  /**
+   * The validation statistics.
+   */
+  override val stats = Statistics.Simple()
+
+  /**
+   * A recurrent neural processor.
+   */
+  private val neuralProcessor = RecurrentNeuralProcessor<NDArrayType>(
+    model = model,
+    useDropout = false,
+    propagateToInput = false)
+
+  /**
+   * Evaluate the model with a single example.
+   *
+   * @param example the example to validate the model with
+   */
+  override fun evaluate(example: SequenceExample<NDArrayType>) {
+
+    val output: DenseNDArray =
+      this.neuralProcessor.forward(example.sequenceFeatures, saveContributions = this.saveContributions)
+
+    val isCorrect: Boolean =
+      this.outputEvaluationFunction(output = output, outputGold = example.sequenceOutputGold.last())
+
+    if (isCorrect)
+      this.stats.metric.truePos++
+    else
+      this.stats.metric.falsePos++
+
+    this.stats.accuracy = this.stats.metric.precision
+
+    this.afterEachEvaluation(example, isCorrect, this.neuralProcessor)
+  }
+}
