c33f55ff201c0e7844b96bcdf97b0aa2d4a5c9a8
KotlinNLP Authors <github@kotlinnlp.com>
2019-12-12 19:27:08 +0100

    Remove unuseful parameter

--- src/main/kotlin/com/kotlinnlp/simplednn/core/neuralnetwork/preset/LTMNeuralNetwork.kt
+++ src/main/kotlin/com/kotlinnlp/simplednn/core/neuralnetwork/preset/LTMNeuralNetwork.kt
@@ -22,7 +22,6 @@ object LTMNeuralNetwork {
    * @param inputSize the size of the input layer (equal to the hidden layers)
    * @param inputType the type of the input layer (Dense -default-, Sparse, SparseBinary)
    * @param inputDropout the dropout probability of the input (default 0.0). If applying it, the usual value is 0.25.
-   * @param hiddenActivation the activation function of the hidden layers
    * @param hiddenDropout the dropout probability of the hidden layers (default 0.0)
    * @param numOfHidden the number of hidden layers (must be >= 0, default 1)
    * @param outputSize the size of the output layer
@@ -33,7 +32,6 @@ object LTMNeuralNetwork {
   operator fun invoke(inputSize: Int,
                       inputType: LayerType.Input = LayerType.Input.Dense,
                       inputDropout: Double = 0.0,
-                      hiddenActivation: ActivationFunction?,
                       hiddenDropout: Double = 0.0,
                       numOfHidden: Int = 1,
                       outputSize: Int,
@@ -45,7 +43,7 @@ object LTMNeuralNetwork {
       inputType = inputType,
       inputDropout = inputDropout,
       hiddenSize = inputSize,
-      hiddenActivation = hiddenActivation,
+      hiddenActivation = null,
       hiddenDropout = hiddenDropout,
       hiddenConnection = LayerType.Connection.LTM,
       numOfHidden = numOfHidden,
