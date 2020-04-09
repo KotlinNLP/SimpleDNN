8571abe60cd143a7bef7d7764a5c8f6831f768f1
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-04-09 20:12:38 +0200

    Add BERT

---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/transformers/BERT.kt
@@ -0,0 +1,350 @@
+/* Copyright 2020-present Simone Cangialosi. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.transformers
+
+import com.kotlinnlp.simplednn.core.arrays.AugmentedArray
+import com.kotlinnlp.simplednn.core.layers.LayerType
+import com.kotlinnlp.simplednn.core.layers.models.attention.scaleddot.ScaledDotAttentionLayer
+import com.kotlinnlp.simplednn.core.layers.models.feedforward.simple.FeedforwardLayer
+import com.kotlinnlp.simplednn.core.layers.models.feedforward.simple.FeedforwardLayersPool
+import com.kotlinnlp.simplednn.core.layers.models.merge.concatff.ConcatFFLayer
+import com.kotlinnlp.simplednn.core.layers.models.merge.concatff.ConcatFFLayersPool
+import com.kotlinnlp.simplednn.core.layers.models.merge.sum.SumLayer
+import com.kotlinnlp.simplednn.core.layers.models.merge.sum.SumLayersPool
+import com.kotlinnlp.simplednn.core.neuralprocessor.NeuralProcessor
+import com.kotlinnlp.simplednn.core.optimizer.ParamsErrorsAccumulator
+import com.kotlinnlp.simplednn.core.optimizer.ParamsErrorsList
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import kotlin.math.pow
+import kotlin.math.sin
+
+/**
+ * A Bidirectional Encoder Representations from Transformers.
+ *
+ * @property model the parameters of the model of the network
+ * @property inputType the type of the input arrays
+ * @property dropout the probability of dropout (default 0.0). If applying it, the usual value is 0.5 (better 0.25 if
+ *                   it's the first layer).
+ * @property propagateToInput whether to propagate the errors to the input during the [backward]
+ * @property id a unique ID
+ */
+class BERT(
+  val model: BERTParameters,
+  val inputType: LayerType.Input,
+  val dropout: Double = 0.0,
+  override val propagateToInput: Boolean = false,
+  override val id: Int
+) : NeuralProcessor<
+  List<DenseNDArray>, // InputType
+  List<DenseNDArray>, // OutputType
+  List<DenseNDArray>, // ErrorsType
+  List<DenseNDArray> // InputErrorsType
+  > {
+
+  /**
+   * Whether to apply the dropout during the [forward].
+   */
+  override val useDropout: Boolean = this.dropout > 0.0
+
+  /**
+   * The errors accumulator.
+   */
+  private val errorsAccumulator = ParamsErrorsAccumulator()
+
+  /**
+   * The input sequence.
+   */
+  private lateinit var inputSequence: List<AugmentedArray<DenseNDArray>>
+
+  /**
+   * The scaled-dot attention layers used for the last forward.
+   */
+  private lateinit var attentionLayers: List<ScaledDotAttentionLayer>
+
+  /**
+   * The pool of concat layers of the multi-head attention outputs.
+   */
+  private val multiHeadMergePool = ConcatFFLayersPool<DenseNDArray>(
+    params = this.model.multiHeadMerge,
+    inputType = LayerType.Input.Dense)
+
+  /**
+   * The concat layers of the multi-head attention outputs that have been used for the last forward.
+   */
+  private lateinit var multiHeadConcatLayers: List<ConcatFFLayer<DenseNDArray>>
+
+  /**
+   * The pool of sum layers for the concatenated multi-head attention outputs.
+   */
+  private val multiHeadSumPool = SumLayersPool<DenseNDArray>(params = this.model.sum, inputType = LayerType.Input.Dense)
+
+  /**
+   * The sum layers for the merged multi-head attention outputs that have been used for the last forward.
+   */
+  private lateinit var multiHeadSumLayers: List<SumLayer<DenseNDArray>>
+
+  /**
+   * The pool of output feed-forward layers.
+   */
+  private val outputFFPool = FeedforwardLayersPool<DenseNDArray>(
+    params = this.model.outputFF,
+    inputType = LayerType.Input.Dense,
+    activationFunction = null)
+
+  /**
+   * The output feed-forward layers that have been used for the last forward.
+   */
+  private lateinit var outputFFLayers: List<FeedforwardLayer<DenseNDArray>>
+
+  /**
+   * The pool of sum layers for the feed-forward outputs.
+   */
+  private val outputSumPool = SumLayersPool<DenseNDArray>(params = this.model.sum, inputType = LayerType.Input.Dense)
+
+  /**
+   * The sum layers for the feed-forward outputs that have been used for the last forward.
+   */
+  private lateinit var outputSumLayers: List<SumLayer<DenseNDArray>>
+
+  /**
+   * The error of the norm scalar parameter accumulated during the last backward.
+   */
+  private var normScalarError: Double = 0.0
+
+  /**
+   * @param input the input sequence
+   *
+   * @return the encoded sequence
+   */
+  override fun forward(input: List<DenseNDArray>): List<DenseNDArray> {
+
+    this.setInputSequence(input)
+
+    return this.forwardOutput(attentionArrays = this.forwardAttention())
+  }
+
+  /**
+   * Propagate the output errors using the gradient descent algorithm.
+   *
+   * @param outputErrors the output errors
+   */
+  override fun backward(outputErrors: List<DenseNDArray>) {
+
+    this.normScalarError = 0.0
+
+    this.backwardAttention(attentionErrors = this.backwardOutput(outputErrors))
+
+    if (this.propagateToInput)
+      this.backwardInput()
+
+    this.errorsAccumulator.accumulate(
+      this.model.normScalarParam.buildDenseErrors(DenseNDArrayFactory.arrayOf(doubleArrayOf(this.normScalarError))))
+  }
+
+  /**
+   * Return the params errors of the last backward.
+   *
+   * @param copy a Boolean indicating whether the returned errors must be a copy or a reference (default true)
+   *
+   * @return the parameters errors
+   */
+  override fun getParamsErrors(copy: Boolean): ParamsErrorsList =
+    this.errorsAccumulator.getParamsErrors(copy = copy)
+
+  /**
+   * Return the input errors of the last backward.
+   * Before calling this method make sure that [propagateToInput] is enabled.
+   *
+   * @param copy whether to return by value or by reference (default true)
+   *
+   * @return the input errors
+   */
+  override fun getInputErrors(copy: Boolean): List<DenseNDArray> =
+    this.inputSequence.map { it.errors }
+
+  /**
+   * Set the input sequence combining it with the positional encoding.
+   *
+   * @param inputSequence the list of arrays of input
+   */
+  private fun setInputSequence(inputSequence: List<DenseNDArray>) {
+
+    this.inputSequence = this.addPositionalEncodings(inputSequence.map { AugmentedArray(it) })
+
+    this.attentionLayers = List(
+      size = this.model.multiHeadStack,
+      init = { ScaledDotAttentionLayer(inputArrays = this.inputSequence, params = this.model.attention) }
+    )
+
+    this.multiHeadMergePool.releaseAll()
+    this.multiHeadConcatLayers = inputSequence.indices.map { this.multiHeadMergePool.getItem() }
+
+    this.multiHeadSumPool.releaseAll()
+    this.multiHeadSumLayers = inputSequence.indices.map { this.multiHeadSumPool.getItem() }
+
+    this.outputFFPool.releaseAll()
+    this.outputFFLayers = inputSequence.indices.map { this.outputFFPool.getItem() }
+
+    this.outputSumPool.releaseAll()
+    this.outputSumLayers = inputSequence.indices.map { this.outputSumPool.getItem() }
+  }
+
+  /**
+   * Add positional encodings to the input sequence.
+   *
+   * @param inputSequence the input sequence
+   *
+   * @return the input sequence with the positional encodings added in-place
+   */
+  private fun addPositionalEncodings(
+    inputSequence: List<AugmentedArray<DenseNDArray>>
+  ): List<AugmentedArray<DenseNDArray>> {
+
+    inputSequence.forEachIndexed { pos, array ->
+
+      (0 .. array.values.length / 2).forEach { i ->
+
+        array.values[i] = sin(pos / 10000.0.pow(2.0 * i / this.model.inputSize))
+
+        if ((i + 1) < array.values.length)
+          array.values[i + 1] = sin(pos / 10000.0.pow((2.0 * i + 1.0) / this.model.inputSize))
+      }
+    }
+
+    return inputSequence
+  }
+
+  /**
+   * The attention component forward.
+   *
+   * @return the output arrays of the attention
+   */
+  private fun forwardAttention(): List<DenseNDArray> {
+
+    this.attentionLayers.forEach { it.forward() }
+
+    return this.multiHeadConcatLayers.zip(this.multiHeadSumLayers).mapIndexed { i, (concatLayer, sumLayer) ->
+
+      concatLayer.inputArrays.zip(this.attentionLayers).forEach { (mergeInput, attentionLayer) ->
+        mergeInput.assignValues(attentionLayer.outputArrays[i].values)
+      }
+      concatLayer.forward()
+
+      sumLayer.inputArrays[0].assignValues(this.inputSequence[i].values)
+      sumLayer.inputArrays[1].assignValues(concatLayer.outputArray.values)
+      sumLayer.forward()
+
+      sumLayer.outputArray.values.prod(this.model.normScalar)
+    }
+  }
+
+  /**
+   * The output component forward.
+   *
+   * @param attentionArrays the attention arrays
+   *
+   * @return the output arrays
+   */
+  private fun forwardOutput(attentionArrays: List<DenseNDArray>): List<DenseNDArray> =
+
+    this.inputSequence.indices.map { i ->
+
+      val outputLayer: FeedforwardLayer<DenseNDArray> = this.outputFFLayers[i]
+      val attentionArray: DenseNDArray = attentionArrays[i]
+      val sumLayer: SumLayer<DenseNDArray> = this.outputSumLayers[i]
+
+      outputLayer.apply { setInput(attentionArray); forward() }
+
+      sumLayer.inputArrays[0].assignValues(attentionArray)
+      sumLayer.inputArrays[1].assignValues(outputLayer.outputArray.values)
+      sumLayer.forward()
+
+      sumLayer.outputArray.values.prod(this.model.normScalar)
+    }
+
+  /**
+   * The output component backward.
+   *
+   * @param outputErrors the output errors
+   *
+   * @return the errors of the attention arrays
+   */
+  private fun backwardOutput(outputErrors: List<DenseNDArray>): List<DenseNDArray> =
+
+    outputErrors.mapIndexed { i, errors ->
+
+      val outputLayer: FeedforwardLayer<DenseNDArray> = this.outputFFLayers[i]
+      val sumLayer: SumLayer<DenseNDArray> = this.outputSumLayers[i]
+
+      this.normScalarError += errors.prod(sumLayer.outputArray.values).sum()
+
+      sumLayer.setErrors(errors.prod(this.model.normScalar))
+      this.errorsAccumulator.accumulate(sumLayer.backward(propagateToInput = true))
+
+      val sumErrors: List<DenseNDArray> = sumLayer.getInputErrors(copy = false)
+      outputLayer.setErrors(sumErrors[1])
+      this.errorsAccumulator.accumulate(outputLayer.backward(propagateToInput = true))
+
+      sumErrors[0].sum(outputLayer.inputArray.errors)
+    }
+
+  /**
+   * The attention component backward.
+   *
+   * @param attentionErrors the errors of the attention arrays
+   */
+  private fun backwardAttention(attentionErrors: List<DenseNDArray>) {
+
+    attentionErrors.mapIndexed { i, errors ->
+
+      val concatLayer: ConcatFFLayer<DenseNDArray> = this.multiHeadConcatLayers[i]
+      val sumLayer: SumLayer<DenseNDArray> = this.multiHeadSumLayers[i]
+
+      this.normScalarError += errors.prod(sumLayer.outputArray.values).sum()
+
+      sumLayer.setErrors(errors.prod(this.model.normScalar))
+      this.errorsAccumulator.accumulate(sumLayer.backward(propagateToInput = true))
+
+      val sumErrors: List<DenseNDArray> = sumLayer.getInputErrors(copy = false)
+      concatLayer.setErrors(sumErrors[1])
+      this.errorsAccumulator.accumulate(concatLayer.backward(propagateToInput = true))
+
+      this.attentionLayers.zip(concatLayer.getInputErrors(copy = false)).forEach { (attentionLayer, concatErrors) ->
+        attentionLayer.outputArrays[i].assignErrors(concatErrors)
+      }
+
+      if (this.propagateToInput)
+        this.inputSequence[i].assignErrors(sumErrors[0])
+    }
+
+    this.attentionLayers.forEach {
+      this.errorsAccumulator.accumulate(it.backward(propagateToInput = this.propagateToInput))
+    }
+  }
+
+  /**
+   * Back-propagation to the input.
+   */
+  private fun backwardInput() {
+
+    this.attentionLayers.fold(mutableListOf<DenseNDArray>()) { errorsList, layer ->
+
+      val attentionInputErrors: Sequence<DenseNDArray> = layer.inputArrays.asSequence().map { it.errors }
+
+      errorsList.apply {
+        if (isEmpty())
+          addAll(attentionInputErrors)
+        else
+          asSequence().zip(attentionInputErrors).forEach { (inputErrors, attentionInputErrors) ->
+            inputErrors.assignSum(attentionInputErrors)
+          }
+      }
+    }
+  }
+}
---
+++ src/main/kotlin/com/kotlinnlp/simplednn/deeplearning/transformers/BERTParameters.kt
@@ -0,0 +1,88 @@
+/* Copyright 2020-present Simone Cangialosi. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.simplednn.deeplearning.transformers
+
+import com.kotlinnlp.simplednn.core.arrays.ParamsArray
+import com.kotlinnlp.simplednn.core.functionalities.initializers.GlorotInitializer
+import com.kotlinnlp.simplednn.core.functionalities.initializers.Initializer
+import com.kotlinnlp.simplednn.core.layers.models.attention.scaleddot.ScaledDotAttentionLayerParameters
+import com.kotlinnlp.simplednn.core.layers.models.feedforward.simple.FeedforwardLayerParameters
+import com.kotlinnlp.simplednn.core.layers.models.merge.concatff.ConcatFFLayerParameters
+import com.kotlinnlp.simplednn.core.layers.models.merge.sum.SumLayerParameters
+import java.io.Serializable
+
+/**
+ * The BERT parameters.
+ *
+ * @property inputSize the size of the input arrays
+ * @property attentionSize the size of the attention arrays
+ * @property hiddenSize the size of the hidden arrays, after the attention
+ * @property multiHeadStack the number of scaled-dot attention layers
+ * @param weightsInitializer the initializer of the weights (zeros if null, default: Glorot)
+ * @param biasesInitializer the initializer of the biases (zeros if null, default: Glorot)
+ */
+class BERTParameters(
+  val inputSize: Int,
+  val attentionSize: Int,
+  val hiddenSize: Int,
+  val multiHeadStack: Int,
+  weightsInitializer: Initializer? = GlorotInitializer(),
+  biasesInitializer: Initializer? = GlorotInitializer()
+) : Serializable {
+
+  companion object {
+
+    /**
+     * Private val used to serialize the class (needed by Serializable).
+     */
+    @Suppress("unused")
+    private const val serialVersionUID: Long = 1L
+  }
+
+  /**
+   * The parameters of the scaled-dot attention layer.
+   */
+  val attention = ScaledDotAttentionLayerParameters(
+    inputSize = this.inputSize,
+    attentionSize = this.attentionSize,
+    outputSize = this.hiddenSize,
+    weightsInitializer = weightsInitializer)
+
+  /**
+   * The parameters of the merge layer of the multi-head attention outputs.
+   */
+  val multiHeadMerge = ConcatFFLayerParameters(
+    inputsSize = List(size = this.multiHeadStack, init = { this.hiddenSize }),
+    outputSize = this.inputSize,
+    weightsInitializer = weightsInitializer,
+    biasesInitializer = biasesInitializer)
+
+  /**
+   * The parameters of the output feed-forward layer.
+   */
+  val outputFF = FeedforwardLayerParameters(
+    inputSize = this.inputSize,
+    outputSize = this.inputSize,
+    weightsInitializer = weightsInitializer,
+    biasesInitializer = biasesInitializer)
+
+  /**
+   * The sum layer parameters.
+   */
+  val sum = SumLayerParameters(inputSize = this.inputSize, nInputs = 2)
+
+  /**
+   * The updatable normalization scalar parameter.
+   */
+  var normScalarParam = ParamsArray(doubleArrayOf(0.0))
+
+  /**
+   * The scalar parameter used to normalize the merged vectors.
+   */
+  val normScalar: Double get() = this.normScalarParam.values[0]
+}
