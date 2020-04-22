8b55d666ca797b274e1a625b50ada4c477da4e70
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-04-22 22:00:40 +0200

    Move files

--- examples/bert/training/CommandLineArguments.kt
+++ examples/bert/training/CommandLineArguments.kt
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * -----------------------------------------------------------------------------*/
 
-package bert
+package bert.training
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
--- examples/bert/training/TrainBERT.kt
+++ examples/bert/training/TrainBERT.kt
@@ -5,7 +5,7 @@
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  * ------------------------------------------------------------------*/
 
-package bert
+package bert.training
 
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizer
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizerModel
