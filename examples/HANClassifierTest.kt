b209fb63ab7f5be60c504d67f7c4c7ef9f783fc2
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-05-20 22:09:06 +0200

    Implement Trainer and Evaluator

--- examples/HANClassifierTest.kt
+++ examples/HANClassifierTest.kt
@@ -7,16 +7,19 @@
 
 import com.kotlinnlp.simplednn.core.functionalities.activations.Softmax
 import com.kotlinnlp.simplednn.core.functionalities.activations.Tanh
-import com.kotlinnlp.simplednn.core.functionalities.updatemethods.adam.ADAMMethod
 import com.kotlinnlp.simplednn.core.layers.LayerType
 import com.kotlinnlp.simplednn.deeplearning.attention.han.HAN
 import com.kotlinnlp.simplednn.deeplearning.attention.han.HANEncoder
 import com.kotlinnlp.simplednn.deeplearning.attention.han.toHierarchySequence
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
-import com.kotlinnlp.utils.progressindicator.ProgressIndicatorBar
 import com.kotlinnlp.simplednn.core.embeddings.EmbeddingsMap
+import com.kotlinnlp.simplednn.core.functionalities.outputevaluation.ClassificationEvaluation
+import com.kotlinnlp.simplednn.core.functionalities.updatemethods.UpdateMethod
+import com.kotlinnlp.simplednn.core.functionalities.updatemethods.radam.RADAMMethod
 import com.kotlinnlp.simplednn.core.optimizer.ParamsOptimizer
-import com.kotlinnlp.utils.ShuffledIndices
+import com.kotlinnlp.simplednn.helpers.Evaluator
+import com.kotlinnlp.simplednn.helpers.Statistics
+import com.kotlinnlp.simplednn.helpers.Trainer
 import com.kotlinnlp.utils.Shuffler
 import utils.Corpus
 import utils.SimpleExample
@@ -25,228 +28,154 @@ import utils.exampleextractor.ClassificationExampleExtractor
 import kotlin.math.roundToInt
 
 /**
- * Train a HAN classifier.
+ * Train a HAN classifier for a sentiment classification (2 classes) and validate it.
  */
 fun main() {
 
   println("Start 'HAN Classifier Test'")
 
-  val dataset = CorpusReader<SimpleExample<DenseNDArray>>().read(
+  val dataset: Corpus<SimpleExample<DenseNDArray>> = CorpusReader<SimpleExample<DenseNDArray>>().read(
     corpusPath = Configuration.loadFromFile().han_classifier.datasets_paths, // same for validation and test
     exampleExtractor = ClassificationExampleExtractor(outputSize = 2),
     perLine = false)
 
-  HANClassifierTest(dataset).start()
+  val trainingSize: Int = (0.9 * dataset.training.size).roundToInt()
+  val trainingSet: List<SimpleExample<DenseNDArray>> = dataset.training.subList(0, trainingSize)
+  val validationSet: List<SimpleExample<DenseNDArray>> = dataset.training.subList(trainingSize, dataset.training.size)
 
-  println("End.")
+  val embeddings = EmbeddingsMap<Int>(size = 50)
+  val model = HAN(
+    hierarchySize = 1,
+    inputSize = embeddings.size,
+    inputType = LayerType.Input.Dense,
+    biRNNsActivation = Tanh,
+    biRNNsConnectionType = LayerType.Connection.RAN,
+    attentionSize = embeddings.size,
+    outputSize = 2,
+    outputActivation = Softmax())
+
+  println("\n-- TRAINING")
+  println("Using %d/%d examples as training set and %d/%d as validation set.".format(
+    trainingSet.size, dataset.training.size, validationSet.size, dataset.training.size))
+
+  HANClassifierTrainer(
+    model = model,
+    examples = trainingSet,
+    embeddings = embeddings,
+    evaluator = HANClassifierEvaluator(model = model, examples = validationSet, embeddings = embeddings),
+    updateMethod = RADAMMethod(stepSize = 0.001)
+  ).train()
+
+  println("\n-- TEST")
+  val stats: Statistics =
+    HANClassifierEvaluator(model = model, examples = dataset.test, embeddings = embeddings).evaluate()
+  println("Accuracy: %.2f%%".format(100.0 * stats.accuracy))
 }
 
 /**
- * Train a HAN classifier with the IMDB movies dataset of encoded documents within a task of sentiment classification (2
- * classes) and validate it.
+ * The HAN classifier trainer.
  */
-class HANClassifierTest(val dataset: Corpus<SimpleExample<DenseNDArray>>) {
-
-  /**
-   * The partition of training set used to train the classifier (the remaining part is used as validation set).
-   */
-  private val trainingSetPartition: Double = 0.9
-
-  /**
-   * The number of epochs for the training.
-   */
-  private val epochs: Int = 10
-
-  /**
-   * The size of the embeddings (used also for the attention arrays).
-   */
-  private val embeddingsSize: Int = 50
-
-  /**
-   * The embeddings associated to each token.
-   */
-  private val embeddings = EmbeddingsMap<Int>(size = this.embeddingsSize)
+private class HANClassifierTrainer(
+  model: HAN,
+  examples: List<SimpleExample<DenseNDArray>>,
+  private val embeddings: EmbeddingsMap<Int>,
+  evaluator: HANClassifierEvaluator,
+  updateMethod: UpdateMethod<*>
+) : Trainer<SimpleExample<DenseNDArray>>(
+  modelFilename = "",
+  optimizers = listOf(ParamsOptimizer(updateMethod)),
+  examples = examples,
+  epochs = 10,
+  batchSize = 1,
+  evaluator = evaluator,
+  shuffler = Shuffler(),
+  verbose = true
+) {
 
   /**
    * The [HANEncoder] used as classifier (Softmax output activation).
    */
-  private val classifier: HANEncoder<DenseNDArray> = this.buildClassifier()
+  private val classifier = HANEncoder<DenseNDArray>(model = model, useDropout = false, propagateToInput = false)
 
   /**
-   * When timing started.
-   */
-  private var startTime: Long = 0
-
-  /**
-   * Start the test.
-   */
-  fun start() {
-
-    println("\n-- TRAINING")
-    this.train()
-
-    println("\n-- TEST")
-    this.validate(validationSet = this.dataset.test)
-  }
-
-  /**
-   * @return the HAN classifier
-   */
-  private fun buildClassifier(): HANEncoder<DenseNDArray> {
-
-    val model = HAN(
-      hierarchySize = 1,
-      inputSize = this.embeddingsSize,
-      inputType = LayerType.Input.Dense,
-      biRNNsActivation = Tanh,
-      biRNNsConnectionType = LayerType.Connection.RAN,
-      attentionSize = this.embeddingsSize,
-      outputSize = 2,
-      outputActivation = Softmax())
-
-    return HANEncoder(model = model, useDropout = false, propagateToInput = false)
-  }
-
-  /**
-   * Train the HAN classifier, validating each epoch.
-   */
-  private fun train() {
-
-    val optimizer = ParamsOptimizer(updateMethod = ADAMMethod(stepSize = 0.005))
-    val shuffler = Shuffler(enablePseudoRandom = true, seed = 743)
-    val trainingSize = (this.dataset.training.size * this.trainingSetPartition).roundToInt()
-    val trainingSet = ArrayList(this.dataset.training.subList(0, trainingSize))
-    val validationSet = ArrayList(this.dataset.training.subList(trainingSize, this.dataset.training.size))
-
-    println("Using %d/%d examples as training set and %d/%d as validation set.".format(
-      trainingSize, this.dataset.training.size, this.dataset.training.size - trainingSize, this.dataset.training.size))
-
-    (0 until this.epochs).forEach {
-
-      println("\nEpoch ${it + 1}")
-      this.trainEpoch(optimizer = optimizer, trainingSet = trainingSet, shuffler = shuffler)
-
-      println("Epoch validation")
-      this.validate(validationSet = validationSet)
-    }
-  }
-
-  /**
-   * Train the HAN classifier over one epoch.
+   * Learn from an example (forward + backward).
    *
-   * @param optimizer the optimizer for the classifier
-   * @param trainingSet the training set
-   * @param shuffler the [Shuffler] to shuffle examples before training
+   * @param example the example used to train the network
    */
-  private fun trainEpoch(optimizer: ParamsOptimizer,
-                         trainingSet: ArrayList<SimpleExample<DenseNDArray>>,
-                         shuffler: Shuffler) {
+  override fun learnFromExample(example: SimpleExample<DenseNDArray>) {
 
-    val progress = ProgressIndicatorBar(trainingSet.size)
-
-    this.startTiming()
-
-    for (exampleIndex in ShuffledIndices(size = trainingSet.size, shuffler = shuffler)) {
-
-      progress.tick()
-
-      val example = trainingSet[exampleIndex]
-      val inputSequence = this.extractInputSequence(example)
-
-      val output: DenseNDArray = this.classifier.forward(inputSequence.toHierarchySequence())
-      this.classifier.backward(outputErrors = output.assignSub(example.outputGold))
-
-      optimizer.accumulate(this.classifier.getParamsErrors(copy = false))
-      optimizer.update()
-    }
-
-    println("Elapsed time: %s".format(this.formatElapsedTime()))
-  }
-
-  /**
-   * Validate the HAN classifier with the example of the given [validationSet].
-   *
-   * @param validationSet the validation set
-   */
-  private fun validate(validationSet: List<SimpleExample<DenseNDArray>>) {
-
-    var correctPredictions = 0
-
-    val progress = ProgressIndicatorBar(validationSet.size)
-    val exampleIndices = ShuffledIndices(
-      size = validationSet.size,
-      shuffler = Shuffler(enablePseudoRandom = true, seed = 1)
-    )
-
-    this.startTiming()
-
-    for (exampleIndex in exampleIndices) {
-
-      progress.tick()
-
-      correctPredictions += this.validateExample(example = validationSet[exampleIndex])
-    }
-
-    println("Elapsed time: %s".format(this.formatElapsedTime()))
-    println("Accuracy: %.2f%%".format(100.0 * correctPredictions / validationSet.size))
-  }
-
-  /**
-   * Validate the HAN classifier with the given [example].
-   *
-   * @param example an example of the validation dataset
-   *
-   * @return 1 if the prediction is correct, 0 otherwise
-   */
-  private fun validateExample(example: SimpleExample<DenseNDArray>): Int {
-
-    val inputSequence = this.extractInputSequence(example)
+    val inputSequence: Array<DenseNDArray> = extractInputSequence(example = example, embeddings = this.embeddings)
     val output: DenseNDArray = this.classifier.forward(inputSequence.toHierarchySequence())
 
-    return if (this.predictionIsCorrect(output, example.outputGold)) 1 else 0
+    this.classifier.backward(outputErrors = output.assignSub(example.outputGold))
   }
 
   /**
-   * @param example an example of the dataset
+   * Accumulate the errors of the model resulting after the call of [learnFromExample].
+   */
+  override fun accumulateErrors() {
+    this.optimizers.single().accumulate(this.classifier.getParamsErrors(copy = false))
+  }
+
+  /**
+   * Dump the model to file.
+   */
+  override fun dumpModel() {}
+}
+
+/**
+ * The HAN classifier evaluator.
+ *
+ * @param model the model to validate
+ * @param examples a list of examples to validate
+ */
+private class HANClassifierEvaluator(
+  model: HAN,
+  examples: List<SimpleExample<DenseNDArray>>,
+  private val embeddings: EmbeddingsMap<Int>
+) : Evaluator<SimpleExample<DenseNDArray>, Statistics.Simple>(
+  examples = examples,
+  verbose = true
+) {
+
+  /**
+   * The validation statistics.
+   */
+  override val stats: Statistics.Simple = Statistics.Simple()
+
+  /**
+   * The [HANEncoder] for the classification.
+   */
+  private val classifier = HANEncoder<DenseNDArray>(model = model, propagateToInput = false, useDropout = false)
+
+  /**
+   * Evaluate the model with a single example.
    *
-   * @return an array of embeddings vectors associated to each feature (casted to Int) of the [example]
+   * @param example the example to validate the model with
    */
-  private fun extractInputSequence(example: SimpleExample<DenseNDArray>): Array<DenseNDArray> {
+  override fun evaluate(example: SimpleExample<DenseNDArray>) {
 
-    return Array(
-      size = example.features.length,
-      init = { i ->
-        val wordIndex = example.features[i].toInt()
-        this.embeddings.getOrSet(wordIndex).values
-      }
-    )
-  }
+    val inputSequence: Array<DenseNDArray> = extractInputSequence(example = example, embeddings = this.embeddings)
+    val output: DenseNDArray = this.classifier.forward(inputSequence.toHierarchySequence())
 
-  /**
-   * @param output an output prediction of the HAN classifier
-   * @param goldOutput the expected gold output
-   *
-   * @return a Boolean indicating if the [output] matches the [goldOutput]
-   */
-  private fun predictionIsCorrect(output: DenseNDArray, goldOutput: DenseNDArray): Boolean {
-    return output.argMaxIndex() == goldOutput.argMaxIndex()
-  }
+    if (ClassificationEvaluation(output = output, outputGold = example.outputGold))
+      this.stats.metric.truePos++
+    else
+      this.stats.metric.falsePos++
 
-  /**
-   * Start registering time.
-   */
-  private fun startTiming() {
-    this.startTime = System.currentTimeMillis()
-  }
-
-  /**
-   * @return the formatted string with elapsed time in seconds and minutes.
-   */
-  private fun formatElapsedTime(): String {
-
-    val elapsedTime = System.currentTimeMillis() - this.startTime
-    val elapsedSecs = elapsedTime / 1000.0
-
-    return "%.3f s (%.1f min)".format(elapsedSecs, elapsedSecs / 60.0)
+    this.stats.accuracy = this.stats.metric.precision
   }
 }
+
+/**
+ * @param example an example of the dataset
+ * @param embeddings the input embeddings
+ *
+ * @return an array of embeddings vectors associated to each feature (casted to Int) of the [example]
+ */
+private fun extractInputSequence(example: SimpleExample<DenseNDArray>,
+                                 embeddings: EmbeddingsMap<Int>): Array<DenseNDArray> =
+  example.features
+    .toDoubleArray()
+    .map { embeddings.getOrSet(it.toInt()).values }
+    .toTypedArray()
